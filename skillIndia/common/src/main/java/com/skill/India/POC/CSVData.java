c6df94216fa77101a29f42156d7b740790f3e46e
ruchipareek5 <ruchipareek5@gmail.com>
2017-05-23 15:50:05 +0530

    Ruchi Pareek :Added two .java files in POC folder and pom.xml to import CSv file using Opencsv

---
+++ skillIndia/common/src/main/java/com/skill/India/POC/CSVData.java
@@ -0,0 +1,31 @@
+package com.skill.India.POC;
+
+public class CSVData {
+
+	public int candidateID;
+	public String candidateName = "";
+	
+	public String getCandidateName() {
+		return candidateName;
+	}
+	public void setCandidateName(String candidateName) {
+		this.candidateName = candidateName;
+	}
+	public int candidateEnrollmentNumber;
+	//public String candidateGender = "";
+	
+	public int getCandidateID() {
+		return candidateID;
+	}
+	public void setCandidateID(int candidateID) {
+		this.candidateID = candidateID;
+	}
+	public int getCandidateEnrollmentNumber() {
+		return candidateEnrollmentNumber;
+	}
+	public void setCandidateEnrollmentNumber(int candidateEnrollmentNumber) {
+		this.candidateEnrollmentNumber = candidateEnrollmentNumber;
+	}  
+
+
+}
---
+++ skillIndia/common/src/main/java/com/skill/India/POC/OpenCSVController.java
@@ -0,0 +1,69 @@
+package com.skill.India.POC;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import au.com.bytecode.opencsv.CSVReader;
+import au.com.bytecode.opencsv.bean.ColumnPositionMappingStrategy;
+import au.com.bytecode.opencsv.bean.CsvToBean;
+import au.com.bytecode.opencsv.bean.HeaderColumnNameTranslateMappingStrategy;
+
+@Controller
+public class OpenCSVController {
+	@RequestMapping("/importcsv")
+	public String ImportCSV() {
+		CSVReader csvReader = null;
+		//HeaderColumnNameTranslateMappingStrategy strategy = new HeaderColumnNameTranslateMappingStrategy();
+		try{
+			//HeaderColumnNameTranslateMappingStrategy strategy = new HeaderColumnNameTranslateMappingStrategy();
+			ColumnPositionMappingStrategy strategy = new ColumnPositionMappingStrategy();  
+			strategy.setType(CSVData.class);
+			//Map<String, String> columnMapping = new HashMap<String, String>();
+			//columnMapping.put("Candidate ID ","candidateID"); 
+			//columnMapping.put("Candidate Name","candidateName");
+			//columnMapping.put("Enrollment Number","candidateEnrollmentNumber");
+			//columnMapping.put("Gender","candidateGender");
+			String[] columns = new String[]{"candidateID","candidateName","candidateEnrollmentNumber"};
+			strategy.setColumnMapping(columns);
+			//strategy.setColumnMapping(columnMapping);
+		
+			String csvFileName = "D:\\Myworkspace\\Sample.csv";
+			csvReader = new CSVReader(new FileReader(csvFileName),',','"',1);
+			//csvReader = new CSVReader(new FileReader(csvFileName),',','"',1);
+			CsvToBean<CSVData> csv = new CsvToBean<CSVData>();
+			List<CSVData> csvDataList = csv.parse(strategy, csvReader);	
+			for(CSVData csvdata: csvDataList){
+				System.out.println("Candidate Id -"+csvdata.getCandidateID());
+				System.out.println("Candidate Enrollment Number -"+csvdata.getCandidateEnrollmentNumber());
+				System.out.println("Candidate Name"+csvdata.getCandidateName());
+				//System.out.println("Candidate Gender"+ csvdata.getCandidateGender());
+			return "ID: "+csvdata.getCandidateID();
+		}
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			try
+			{
+				//closing the reader
+				csvReader.close();
+				return "importCSV";
+			}
+			catch(Exception ee)
+			{
+				ee.printStackTrace();
+			}
+		}
+	return "importCSV";
+	}
+
+}
