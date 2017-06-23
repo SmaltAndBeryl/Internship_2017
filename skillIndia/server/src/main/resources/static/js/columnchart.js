89e6c8e79797aac45cc653d984f48de261763ca9
prateek-kapoor323 <prateek@smaltandberyl.com>
2017-06-23 09:16:26 +0530

    Author         :Prateek Kapoor Stody ID       :SSDMS-35 Files Added    :columnchart.js Description    :JS file for columnchart having a url for JSON Object

---
+++ skillIndia/server/src/main/resources/static/js/columnchart.js
@@ -0,0 +1,44 @@
+        $(function () {
+                var processed_json = new Array();   
+            
+      $.getJSON('http://localhost/high_charts/data.json', function(data) {                                                  
+          for (i = 0; i < data.length; i++){
+                        processed_json.push([data[i].key, data[i].value]);
+                    }
+                 
+                    // draw chart
+                    $('#container').highcharts({
+                    chart: {
+                        type: "column"
+                    },
+                    title: {
+                        text: ""
+                    },
+                    xAxis: {
+                        type: 'category',
+                        allowDecimals: false,
+                        title: {
+                            text: "States"
+                        }
+                    },
+                    yAxis: {
+                        title: {
+                            text: "Total Training Centres"
+                        }
+                    },
+                        credits: {
+            enabled: false
+        },
+                    series: [{
+                        name: 'Training Centres',
+                        data: processed_json
+                    }]
+                }); 
+                                                                                    
+                                                                                    
+                                                                              
+                                                                                    
+                                                                                    
+            });
+        });
+    
