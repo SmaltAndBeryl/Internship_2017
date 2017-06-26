994d9ead4ddafe2c5c03da6d489e08eb36281c3f
Alkesh <alkeshsrivastava11@gmail.com>
2017-06-26 15:50:57 +0530

    Author            :Alkesh Srivastava Story ID          :L-02, SSDMS-28 Tasks             :Design a master template Files Added       :Master Template POC Files Modified    :None Files Deleted     :None Description       :Added files for QA

---
+++ skillIndia/server/src/main/resources/static/poc/Master Template POC/AngTest/js/route.js
@@ -0,0 +1,25 @@
+// var appRoute = angular.module('appRoute', [ngRoute]);
+//
+// appRoute.config(function($routeProvider) {
+//   $routeProvider
+//   .when("/dashboard",{
+//     templateUrl: "dashboard.html",
+//     controller: 'dashController'
+//   })
+//   .when("/manage-registrations", {
+//     templateUrl: "",
+//     controller: 'manageController'
+//   })
+//   .when("/import", {
+//     templateUrl: "",
+//     controller: 'importController'
+//   })
+//   .when("/faq", {
+//     templateUrl: "",
+//     controller: 'faqController'
+//   })
+//   .when("/batch-assignment", {
+//     templateUrl: "",
+//     controller: 'batchController'
+//   });
+// });
