54ae62a3d966036d1a47f45f7960afb06e83ae81
poojasharma06 <sharmapooja6797@gmail.com>
2017-06-28 18:36:21 +0530

    Author:        Pooja Sharma Story Id:      SSDMS-38 Files Added:   approve.css, approve.html, approve.js, approveReject.yml Description:   Front-end partials/MR_ApproveRejectTable.html(MR) Code Reviewed: Priyanka Chaudhary

---
+++ skillIndia/server/src/main/resources/static/js/approve.js
@@ -0,0 +1,61 @@
+var app = angular.module('app', [ 'ui.grid', 'ui.grid.edit', 'ui.grid.cellNav']);
+
+app.controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
+ 
+   
+    $scope.deleteRow = function(row) {
+    var index = $scope.gridOptions.data.indexOf(row.entity);
+	//console.log($scope.gridOptions.data[0]);
+	var obj = $scope.gridOptions.data[0];
+	console.log(Object.values(obj));
+		
+		
+	var temp = Object.values(obj)[5];
+	
+	console.log(Object.values(obj)[5]);
+//	console.log(arr[1]);
+    $scope.gridOptions.data.splice(index, 1);
+  };
+	
+//	  $scope.saveRow = function (row) {
+//        var promise = $http.post('data.json');
+//		console.log(promise)
+//        $scope.gridApi.rowEdit.setSavePromise($scope.gridApi.grid, rowEntity, promise.promise);
+//        console.log(rowEntity);
+//		  
+//		  
+//		  onRegisterApi: function (gridApi) {
+//            $scope.gridApi = gridApi;
+//            gridApi.rowEdit.on.saveRow($scope, $scope.saveRow);
+//        }
+		 
+		  
+	 refresh();
+	
+  $scope.gridOptions = {
+	  enableGridMenus : false,  
+	  enableSorting: false, 
+	  enableFiltering: false,
+	  enableCellEdit : false,
+	  
+	  enableColumnMenus : false,
+    columnDefs: [
+		{ name: 'Serial no'},
+      	{ name: 'Institution name | Location', width: 300  },
+		{ name: 'Type'},
+		{ name: 'Date'},
+		{ name: 'View Application'},
+		{ name: 'Comments', enableCellEdit : true},
+		{ name: 'Action', cellTemplate: '<button class="btn primary" ng-click="grid.appScope.deleteRow(row)">&#10004</button>' }
+    ]
+  };
+  
+  function refresh() {
+    $http.get('classpath:sql/approveReject.yml')
+    .success(function (data) {
+      $scope.gridOptions.data = data;
+    });
+  }
+}
+  
+]);
