f82b79f45943d867520dadbc3e8338ab8cc8ca6d
rashidhaka <dhakarashi13@gmail.com>
2017-06-28 18:33:57 +0530

    Author : Rashi dhaka Story id : 44 Files added : data Import.css ,data Import.js,data Import.html Description : HTML and CSS were created for making the page as per GD

---
+++ skillIndia/server/src/main/resources/static/js/controllers/data Import.js
@@ -0,0 +1,30 @@
+var app = angular.module('app', ['ngTouch', 'ui.grid', 'ui.grid.autoResize']);
+
+app.controller('MainCtrl', ['$scope', '$location','$http', '$log', function ($scope,$location, $http, $log) {
+  
+	$scope.gridOptions = {enableColumnMenus : false,
+						enableSorting : false,
+						enableHorizontalScrollbar:0,
+						enableVerticalScrollbar:0};
+
+  $scope.gridOptions.columnDefs = [
+    { name:'S No' , cellClass:'sno'},
+    { name:'file name', cellClass:'fname'},
+    { name:'type', cellClass:'type'},
+    { name:'date', cellClass:'date'},
+    { name:'uploaded by', cellClass:'uby'},
+   { name: 'view uploaded by',cellClass:'vub',cellTemplate: '<img src="img.png" ng-click=grid.appScope.myfunction()>' }
+  ];
+	
+	
+ $scope.myfunction = function() 
+  {  location.href = 'http://www.google.com';
+};
+  $http.get('https://cdn.rawgit.com/angular-ui/ui-grid.info/gh-pages/data/500_complex.json')
+    .success(function(data) {
+      $scope.gridOptions.data = data;
+    });
+
+ }]);
+
+
