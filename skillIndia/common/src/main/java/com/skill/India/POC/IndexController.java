995f0a47c953399a98292bfbb3c915cbe3e8d24a
Alkesh <alkeshsrivastava11@gmail.com>
2017-06-28 20:24:15 +0530

    Author          : Alkesh Srivastava Story ID        : L-02, SSDMS 28 Task            : Added controller class for index page Files Added     : IndexController.java Files Deleted   : None Description     : Create a master template

---
+++ skillIndia/common/src/main/java/com/skill/India/POC/IndexController.java
@@ -0,0 +1,15 @@
+package com.skill.India.POC;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Alkesh srivastav on 6/27/2017.
+ */
+@Controller
+public class IndexController {
+    @RequestMapping("/index")
+    public String index(){
+        return "index2";
+    }
+}
