ce2a01d7abaed2211c58cc952950afd52471c53a
shubham6929 <shubham@smaltandberyl.com>
2017-07-06 19:43:40 +0530

    Author          : Shubham Jain Story ID        : SSDMS-49,51 Task            : Create a dynamic FAQ page Files Added     : FAQ.html,FAQ.js Files Deleted   : none Description     : Updated the HTML & JS for FAQ Page

--- skillIndia/server/src/main/resources/static/js/controllers/FAQ.js
+++ skillIndia/server/src/main/resources/static/js/controllers/FAQ.js
@@ -1,8 +1,8 @@
 var app = angular.module('app', [ 'ui.grid', 'ui.grid.edit', 'ui.grid.cellNav']);
 
-app.controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
- 
-   
+app.controller('FAQScreenController', ['$scope', '$http', function ($scope, $http) {
+ //to fill data in scheme drop down
+   $scope.schemeDropDownOptions = ['PMKVY','Non-PMKVY'];
     $scope.deleteRow = function(row) {
     var index = $scope.gridOptions.data.indexOf(row.entity);
 	var obj = $scope.gridOptions.data[0];
@@ -14,9 +14,7 @@ app.controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
 	console.log(Object.values(obj)[5]);
     $scope.gridOptions.data.splice(index, 1);
   };
-			  
-	 refresh();
-	
+
   $scope.gridOptions = {
 	  enableGridMenus : false,  
 	  enableSorting: false, 
@@ -25,17 +23,32 @@ app.controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
 	  
 	  enableColumnMenus : false,
     columnDefs: [
-		{ name: 'Candidates', width: 400},
-		{ name: 'Total No. Of Candidates', width:500}
+		{ name: 'totalNoOfTrained' diplayname: 'Trained', width: 250},
+		{ displayname: 'Assessed', width:250},
+		{ displayname: 'Certified', width:250}
     ]
   };
-  
-  function refresh() {
-    $http.get('192.168.4.15:8080/getFAQTotalCandidatesTrainedAssessedCertifiedDao')
-    .success(function (data) {
-      $scope.gridOptions.data = data;
-    });
-  }
+  var queryParameter = $scope.schemeDropDownOptions;
+  $scope.getDataOfScheme = function getDataOfScheme() {
+	  var FAQURI = "/192.168.3.29:8080/getFAQTotalCandidatesTrainedAssessedCertifiedSchemeWise?batchType";
+	  console.log($scope.selectedScheme);
+	  console.log('192.168.3.29:8080/getFAQTotalCandidatesTrainedAssessedCertifiedSchemeWise?batchType='+ $scope.schemeDropDownOptions)
+	  $http({
+		  url : FAQURI,
+		  method : "POST",
+		  data : angular.toJson($scope.selectedScheme),
+		  headers : {
+			  'Content-type': 'application/json'
+					}
+		  }).then(function(response)
+					{
+				     console.log(response);
+				     $scope.gridOptions.data = response;
+					});
+	  }
+     
 }
-  
 ]);
+
+
+
