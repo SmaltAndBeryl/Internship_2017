99395d6716e7166830775270c9a4b349d60de8cb
PranjalKumarSingh <pranjal@smaltandberyl.com>
2017-07-07 00:28:33 +0530

    Author      : Pranjal Kumar Singh
    Story Id    : SSDMS 23, 26
    
    Files Added : TpApplicationStatus.js, TpApplicationStatus.html trainingPartnerApplicationStatus.yml, edit1.png
    
    Description  : Files added for Frontend of Training Partner Application Status Table

--- skillIndia/server/src/main/resources/static/js/controllers/TpApplicationStatus.js
+++ skillIndia/server/src/main/resources/static/js/controllers/TpApplicationStatus.js
@@ -1,32 +1,30 @@
 var app = angular.module('app', ['ngTouch', 'ui.grid', 'ui.grid.autoResize']);
-app.controller('MainCtrl', ['$scope', '$http', '$log', function ($scope, $http, $log) {
-  $scope.gridOptions = {enableColumnMenus: false,enableSorting: false,verticalScrollbar:0};
-  $scope.gridOptions.columnDefs = [
-    { name:'Application ID',
-	cellClass:'Prostokt-2-kopia-2-copy-10',headerCellClass:'Application-ID',width:260,
+app.controller('MainCtrl', ['$scope', '$http', '$location', function ($scope, $http, $location) {
+	
+	$scope.gridOptions = {
+		  enableColumnMenus: false,
+		  enableSorting: false,
+		  verticalScrollbar:0,
+		  
+  columnDefs : [
+    { name: 'applicationID' ,	displayName:'Application ID',	cellClass:'Prostokt-2-kopia-2-copy-10',	headerCellClass:'Application-ID',	width:260,
 	},
-    { name:'Date of Submission',
-	cellClass:'Prostokt-2-kopia-2-copy-11',headerCellClass:'Date-of-Submission',width:208,
+    { name: 'dateOfSubmission',	displayName:'Date of Submission',	cellClass:'Prostokt-2-kopia-2-copy-11',	headerCellClass:'Date-of-Submission',	width:208,
 	},
-    { name:'Submitted By',
-	cellClass:'Prostokt-2-kopia-2-copy-12',headerCellClass:'Submitted-By',width:208,
+    { name: 'submittedby' ,	displayName:'Submitted By',	cellClass:'Prostokt-2-kopia-2-copy-12',	headerCellClass:'Submitted-By',	width:208,
 	},
-    { name:'Status',
-	cellClass:'Prostokt-2-kopia-2-copy-13',headerCellClass:'Status',width:185,
+    { name: 'status' ,	displayName:'Status',	cellClass:'Prostokt-2-kopia-2-copy-13',	headerCellClass:'Status',	width:185,
 	},
-    { name:'Action',
-     cellClass: 'Prostokt-2-kopia-2-copy-14',width:76,
-     headerCellClass: 'Action-copy',
-     cellTemplate:'<span class="edit"></span><img src="edit.png" ng-click=grid.appScope.myfunction()>'
+    { name:'Action',	cellClass: 'Prostokt-2-kopia-2-copy-14',	width:76,	headerCellClass: 'Action-copy',	cellTemplate:'<span class="edit"></span><img src="icon/indexpageIcons/edit1.png" ng-click=grid.appScope.myfunction()>',
     },
-    { name:'Comment',
-	  cellClass:'Prostokt-2-kopia-2-copy-15',headerCellClass:'Comment',width:201,
+    { name: 'comment' ,	displayName:'Comment',	cellClass:'Prostokt-2-kopia-2-copy-15',	headerCellClass:'Comment',	width:201,
 	}
-	];
+	]
+  };
 $scope.myfunction = function() 
   {  location.href = 'http://www.google.com';
 };
-$http.get('../poc/Status.json')
+$http.get('/trainingPartnerApplicationStatus')
     .success(function (data) {
       $scope.gridOptions.data = data;
     });
