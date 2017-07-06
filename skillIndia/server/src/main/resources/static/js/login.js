d1857eae89b02d7341d95353883f3055186be63c
VikasMalhotra163 <vikas@smaltandberyl.com>
2017-07-06 13:07:43 +0530

    Author : vikas files added : login.js

--- skillIndia/server/src/main/resources/static/js/login.js
+++ skillIndia/server/src/main/resources/static/js/login.js
@@ -1,3 +1,47 @@
+
+       <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.6/angular.min.js"></script>
+    <script>
+        
+    //Create a module and create a controller for angular
+        var myangularapp = angular.module("myangularapp", []);
+myangularapp.controller("myangularController", function($scope,$http) {
+       
+        
+    $scope.login = function (){
+     alert('test3');
+    
+        $scope.username;
+        $scope.password1;
+        
+        $http.post('http://localhost/loginangulartutorial2/webservices/login.php', { username: $scope.username,
+       password: $scope.password1,
+             }).then(function(response) {
+        alert('hi i am working');
+        //alert(JSON.stringify(response));
+        alert(response.data[0].status);
+            if(response.data[0].status == 1){
+                window.location.href = 'http://joelwebsites.com';
+            }
+            
+    }, function errorCallback(response) {
+            alert('error');
+    // called asynchronously if an error occurs
+    // or server returns response with an error status.
+  });
+    }
+    
+});
+    
+    
+    
+    </script>
+    
+    
+    
+  <script src="http://code.jquery.com/jquery-1.12.0.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <script>
+    
     $(function() {
 
     $('#login-form-link').click(function(e) {
@@ -6,25 +50,35 @@
 		$('#register-form-link').removeClass('active');
 		$(this).addClass('active');
 		e.preventDefault();
-		$("#password").password('toggle');
+		$('#password').password('toggle');
 
 	});
-//	$('#register-form-link').click(function(e) {
-//		$("#register-form").delay(100).fadeIn(100);
-// 		$("#login-form").fadeOut(100);
-//		$('#login-form-link').removeClass('active');
-//		$(this).addClass('active');
-//		e.preventDefault();
-//	});
+	$('#register-form-link').click(function(e) {
+		$("#register-form").delay(100).fadeIn(100);
+ 		$("#login-form").fadeOut(100);
+		$('#login-form-link').removeClass('active');
+		$(this).addClass('active');
+		e.preventDefault();
+	});
 
 });
 
         function login(){
         //login code here
     var username = $('#username').val();
-      
-            $.post(url, function(data, status)
-            		{
+    var password = $('#password1').val();
+            
+    alert(username+'hi i am the username login');
+        alert(password+'hi i am the password login');
+            
+        alert('login function is called!');
+       var url = 'http://localhost/logintutorial/webservices/login.php?username='+username+'&password='+password;
+            
+            alert(url);
+            console.log(url);
+            $.post(url,
+    function(data, status){
+                
                 alert(JSON.stringify(data));
                 var status = data[0].status;
                 
@@ -47,12 +101,18 @@
     var confirmpassword =$('#confirm-password').val();
       
             
-         
+            $.post("http://localhost/logintutorial/webservices/register.php",
     {
-        password=confirmpassword,
-        email=email,
-        username=username
-    }
+        password: confirmpassword,
+        email: email,
+        username: username
+    },
+    function(data, status){
+        alert("Data: " + data + "\nStatus: " + status);
+    });
+        }
+        
+    </script>
+
     
-   };
- 
+   
