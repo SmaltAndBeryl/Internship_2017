db104597ea9fca7b7511574ca844b7cfc956a4c3
shubham6929 <shubham@smaltandberyl.com>
2017-07-07 20:13:00 +0530

    Author          : Shubham Jain Story ID        : SSDMS-49,51 Task            : Create a dynamic FAQ page Files Added     : FAQ.html,FAQ.js Files Deleted   : none Description     : Updated the HTML & JS for FAQ Page

--- skillIndia/server/src/main/resources/static/js/controllers/FAQ.js
+++ skillIndia/server/src/main/resources/static/js/controllers/FAQ.js
@@ -1,19 +1,20 @@
 var app = angular.module('app', [ 'ui.grid', 'ui.grid.edit', 'ui.grid.cellNav']);
 
-app.controller('FAQScreenController', ['$scope', '$http', function ($scope, $http) {
+app.controller('FAQTotalCandidatesTrainedAssessedCertifiedController', ['$scope', '$http', function ($scope, $http) {
  //to fill data in scheme drop down
-   $scope.schemeDropDownOptions = ['PMKVY','Non-PMKVY'];
-    $scope.deleteRow = function(row) {
-    var index = $scope.gridOptions.data.indexOf(row.entity);
-	var obj = $scope.gridOptions.data[0];
-	console.log(Object.values(obj));
-		
-		
-	var temp = Object.values(obj)[5];
-	
-	console.log(Object.values(obj)[5]);
-    $scope.gridOptions.data.splice(index, 1);
-  };
+//    $scope.deleteRow = function(row) {
+//    var index = $scope.gridOptions.data.indexOf(row.entity);
+//	var obj = $scope.gridOptions.data[0];
+//	console.log(Object.values(obj));
+//		
+//		
+//	var temp = Object.values(obj)[5];
+//	
+//	console.log(Object.values(obj)[5]);
+//    $scope.gridOptions.data.splice(index, 1);
+//  };
+  
+    getDataAcrossAllScheme();
 
   $scope.gridOptions = {
 	  enableGridMenus : false,  
@@ -23,32 +24,115 @@ app.controller('FAQScreenController', ['$scope', '$http', function ($scope, $htt
 	  
 	  enableColumnMenus : false,
     columnDefs: [
-		{ name: 'totalNoOfTrained' diplayname: 'Trained', width: 250},
-		{ displayname: 'Assessed', width:250},
-		{ displayname: 'Certified', width:250}
+		{ name: 'totalNoOfCandidatesEnrolled' , displayName: 'Total No. Of Candidate Enrolled', width:250},
+		{ name: 'totalNoOfCandidatesAssessed' , displayName: 'Total No. Of Candidate Assessed', width:250},
+		{ name: 'totalNoOfCandidatesCertified', displayName: 'Total No. Of Candidate Certified', width:250}
     ]
   };
-  var queryParameter = $scope.schemeDropDownOptions;
-  $scope.getDataOfScheme = function getDataOfScheme() {
-	  var FAQURI = "/192.168.3.29:8080/getFAQTotalCandidatesTrainedAssessedCertifiedSchemeWise?batchType";
-	  console.log($scope.selectedScheme);
-	  console.log('192.168.3.29:8080/getFAQTotalCandidatesTrainedAssessedCertifiedSchemeWise?batchType='+ $scope.schemeDropDownOptions)
-	  $http({
-		  url : FAQURI,
-		  method : "POST",
-		  data : angular.toJson($scope.selectedScheme),
-		  headers : {
-			  'Content-type': 'application/json'
-					}
-		  }).then(function(response)
-					{
-				     console.log(response);
-				     $scope.gridOptions.data = response;
-					});
-	  }
+
+  function getDataAcrossAllScheme() {
+  
+	  $http.get('/getFAQTotalCandidatesTrainedAssessedCertified')
+	    .success(function (response) {
+	      $scope.gridOptions.data = response;
+	    });
+  
+  
+       }
      
 }
 ]);
+
+app.controller('FAQCountOfTotalTrainingCentresConductingTrainingController', ['$scope', '$http', function ($scope, $http) {
+	 //to fill data in scheme drop down
+	   $scope.schemeDropDownOptions = ['PMKVY','Non-PMKVY'];
+	    $scope.deleteRow = function(row) {
+	    var index = $scope.gridOptions.data.indexOf(row.entity);
+		var obj = $scope.gridOptions.data[0];
+		console.log(Object.values(obj));
+			
+			
+		var temp = Object.values(obj)[5];
+		
+		console.log(Object.values(obj)[5]);
+	    $scope.gridOptions.data.splice(index, 1);
+	  };
+
+	  $scope.gridOptions = {
+		  enableGridMenus : false,  
+		  enableSorting: false, 
+		  enableFiltering: false,
+		  enableCellEdit : false,
+		  
+		  enableColumnMenus : false,
+	    columnDefs: [
+			{ name: 'Total No. Of Training', width:250}
+	    ]
+	  };
+	  var queryParameter = $scope.schemeDropDownOptions;
+	  $scope.getDataOfScheme = function getDataOfScheme() {
+//		  var FAQURI2 = "/getFAQCountOfTotalTrainingCentresConductingTraining?batchType=";
+		  console.log($scope.selectedScheme);
+		  console.log('/getFAQCountOfTotalTrainingCentresConductingTraining?batchType='+ $scope.selectedScheme)
+		  $http.post("/getFAQCountOfTotalTrainingCentresConductingTraining?batchType=").then(function(scheme)
+						{
+					     console.log(scheme);
+					     $scope.gridOptions.data = scheme;
+						});
+		  }
+	     
+	}
+	]);
+
+
+app.controller('FAQScreenController3', ['$scope', '$http', function ($scope, $http) {
+	 //to fill data in scheme drop down
+	   $scope.stateDropDownOptions = ['Andhra Pradesh','Arunachal Pradesh','Assam','Bihar','Chhattisgarh','Delhi','Goa','Gujarat','Haryana','Himachal Pradesh','Jammu & Kashmir','Jharkhand','Karnataka','Kerala','Madhya Pradesh','Maharastra','Manipur','Meghalaya','Mizoram','Nagaland','Odisha','Punjab','Rajasthan','Sikkim','Tamil Nadu','Telangana','Tripura','Uttarakhand','Uttar Pradesh','West Bengal'];
+//	    $scope.deleteRow = function(row) {
+//	    var index = $scope.gridOptions.data.indexOf(row.entity);
+//		var obj = $scope.gridOptions.data[0];
+//		console.log(Object.values(obj));
+//			
+//			
+//		var temp = Object.values(obj)[5];
+//		
+//		console.log(Object.values(obj)[5]);
+//	    $scope.gridOptions.data.splice(index, 1);
+//	  };
+//
+//	  $scope.gridOptions = {
+//		  enableGridMenus : false,  
+//		  enableSorting: false, 
+//		  enableFiltering: false,
+//		  enableCellEdit : false,
+//		  
+//		  enableColumnMenus : false,
+//	    columnDefs: [
+//			{ name: 'trained', width:250, displayName:'Total candidates trained in a state'}
+//	    ]
+//	  };
+	  var queryParameter = $scope.stateDropDownOptions;
+	  $scope.getDataOfState = function getDataOfState() {
+		  var getTotalTrainingCenterUrl = "/getFAQTotalTrainingCentresInAState?state="+$scope.selectedState;
+		  console.log($scope.selectedState);
+		  console.log(getTotalTrainingCenterUrl);
+		//  console.log('/getFAQTotalTrainingCentresInAState?state='+ $scope.stateDropDownOptions)
+		  $http.post(getTotalTrainingCenterUrl).then(function(state)
+						{
+					     console.log(state);
+					     $scope.state = state.data;
+					      
+					     
+//					     $scope.gridOptions.data = state;
+						});
+		  }
+	     
+	}
+	]);
+
+
+
+
 
 
 
