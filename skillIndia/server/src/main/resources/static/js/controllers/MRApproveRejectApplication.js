1348e0f349e15a7d62a3964d82edcd795687e60d
deepakkandpal6496 <deepak.kandpal@smaltandberyl.com>
2017-07-08 09:52:59 +0530

    Author         :Deepak Kandpal StoryId        :SSDMS-37 Files Modified :MRApproveRejectApplication.js Description    :javascript file for manage registration

--- skillIndia/server/src/main/resources/static/js/controllers/MRApproveRejectApplication.js
+++ skillIndia/server/src/main/resources/static/js/controllers/MRApproveRejectApplication.js
@@ -11,11 +11,11 @@ refresh();
 	  enableColumnMenus : false,
     
 	  columnDefs: [
-		{ name: 'Serial no', displayName: 'Serial No', cellClass:'sno',headerCellClass:'Institution-Name',width:80},
-      	{ name: 'Institution name | Location', displayName: 'Institution Name', width: 190 ,cellClass:'fname',headerCellClass:'Institution-Name' },
-		{ name: 'Type',displayName: 'Type' , cellClass:'Type',headerCellClass:'Institution-Name'},
-		{ name: 'Date', displayName: 'Date',width:100,cellClass:'layer',headerCellClass:'Institution-Name'},
-		{ name: 'View Application', displayName:'View', cellTemplate:'<img src="icon/indexPageIcons/pdf.png" ng-click=grid.appScope.myfunction()>',headerCellClass:'Institution-Name', cellClass:'va',width:120},
+		{ name: 'applicationId', displayName: 'Serial No', cellClass:'sno',headerCellClass:'Institution-Name',width:80},
+      	{ name: 'applicationState', displayName: 'Institution Name', width: 190 ,cellClass:'fname',headerCellClass:'Institution-Name' },
+		{ name: 'activeFlag',displayName: 'Type' , cellClass:'Type',headerCellClass:'Institution-Name'},
+		{ name: 'dateOfSubmission', displayName: 'Date',width:100,cellClass:'layer',headerCellClass:'Institution-Name'},
+		{ name: 'userId', displayName:'View', cellTemplate:'<img src="icon/indexPageIcons/pdf.png" ng-click=grid.appScope.myfunction()>',headerCellClass:'Institution-Name', cellClass:'va',width:120},
 		{ name: 'Comments', displayName:'Comments' ,enableCellEdit: true, width:180,headerCellClass:'Institution-Name',cellClass:'va'},
         { name: 'Action', displayName:'Action' , cellTemplate: '<label><img src="icon/indexPageIcons/edit.png" ng-click=grid.appScope.myfunction()>&nbsp; &nbsp; &nbsp<img src="icon/indexPageIcons/tick.png" ng-click=grid.appScope.myfunction1()>  &nbsp; &nbsp; <img src="icon/indexPageIcons/close.png" ng-click=grid.appScope.myfunction2()></label>',headerCellClass:'Institution-Name',cellClass:'va'}
 		
@@ -33,7 +33,7 @@ refresh();
     };
   
   function refresh() {
-    $http.get('')
+    $http.get('/approve')
     .success(function (data) {
       $scope.gridOptions.data = data;
     });
