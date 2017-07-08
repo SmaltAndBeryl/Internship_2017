99ee024f9d089084dd2c66d420a5ebfd705b8c91
PranjalKumarSingh <pranjal@smaltandberyl.com>
2017-07-08 10:42:40 +0530

    Author       : Pranjal Kumar Singh Story Id     : 15, 21, 23, 26 File Added   : AssessmentBodyController.java Description  : Controller Added for Assessment body application status.

--- skillIndia/common/src/main/java/com/skill/India/controller/AssessmentBodyController.java
+++ skillIndia/common/src/main/java/com/skill/India/controller/AssessmentBodyController.java
@@ -8,26 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//import com.skill.India.dto.AssessmentBodyBatchesDto;
-//import com.skill.India.service.AssessmentBodyBatchesService;
 import com.skill.India.dto.AssessmentBodyApplicationStatusDto;
 import com.skill.India.service.AssessmentBodyApplicationStatusService;
 
 @RestController
 public class AssessmentBodyController {
 	
-//	@Autowired
-//	private AssessmentBodyBatchesService assessmentBodyBatchesService;
-	
 	@Autowired
 	private AssessmentBodyApplicationStatusService assessmentBodyApplicationStatusService;
-	
-	
-//	@RequestMapping("/assessmentBodyBatches")
-//	public HashMap<String, ArrayList<AssessmentBodyBatchesDto>> getAssessmentBodyUpcomingTableDto () {
-//			return assessmentBodyBatchesService.getUpdateRowMapper();
-//	}
-//	
 			
 	@RequestMapping("/assessmentBodyApplicationStatus")
 	public Collection<AssessmentBodyApplicationStatusDto> getAssessmentBodyApplicationStatusDto () {
