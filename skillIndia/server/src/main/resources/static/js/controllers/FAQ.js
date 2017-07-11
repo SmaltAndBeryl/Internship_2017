92f1e5c6c31b7b233f5bcc8b97236c57f0bd21dc
shubham6929 <shubham@smaltandberyl.com>
2017-07-11 11:31:21 +0530

    Author          : Shubham Jain Story ID        : SSDMS-32,34 Task            : Create a dynamic FAQ Page Files Added     : FAQ.js,faq.html Files Deleted   : none Description     : Modified the FAQ.js and faq.html file for demostration

--- skillIndia/server/src/main/resources/static/js/controllers/FAQ.js
+++ skillIndia/server/src/main/resources/static/js/controllers/FAQ.js
@@ -21,7 +21,6 @@ app.controller('FAQTotalCandidatesTrainedAssessedCertifiedController', ['$scope'
 	  enableSorting: false, 
 	  enableFiltering: false,
 	  enableCellEdit : false,
-	  
 	  enableColumnMenus : false,
     columnDefs: [
 		{ name: 'totalNoOfCandidatesEnrolled' , displayName: 'Total No. Of Candidate Enrolled', width:250},
@@ -42,50 +41,31 @@ app.controller('FAQTotalCandidatesTrainedAssessedCertifiedController', ['$scope'
      
 }
 ]);
-
-app.controller('FAQCountOfTotalTrainingCentresConductingTrainingController', ['$scope', '$http', function ($scope, $http) {
-	 //to fill data in scheme drop down
-	   $scope.schemeDropDownOptions = ['PMKVY','Non-PMKVY'];
-	    $scope.deleteRow = function(row) {
-	    var index = $scope.gridOptions.data.indexOf(row.entity);
-		var obj = $scope.gridOptions.data[0];
-		console.log(Object.values(obj));
-			
-			
-		var temp = Object.values(obj)[5];
-		
-		console.log(Object.values(obj)[5]);
-	    $scope.gridOptions.data.splice(index, 1);
-	  };
-
-	  $scope.gridOptions = {
-		  enableGridMenus : false,  
-		  enableSorting: false, 
-		  enableFiltering: false,
-		  enableCellEdit : false,
-		  
-		  enableColumnMenus : false,
-	    columnDefs: [
-			{ name: 'Total No. Of Training', width:250}
-	    ]
-	  };
-	  var queryParameter = $scope.schemeDropDownOptions;
-	  $scope.getDataOfScheme = function getDataOfScheme() {
-//		  var FAQURI2 = "/getFAQCountOfTotalTrainingCentresConductingTraining?batchType=";
-		  console.log($scope.selectedScheme);
-		  console.log('/getFAQCountOfTotalTrainingCentresConductingTraining?batchType='+ $scope.selectedScheme)
-		  $http.post("/getFAQCountOfTotalTrainingCentresConductingTraining?batchType=").then(function(scheme)
-						{
-					     console.log(scheme);
-					     $scope.gridOptions.data = scheme;
-						});
-		  }
-	     
-	}
-	]);
+// For Total Candidates Trained Assessed Certified Scheme Wise Controller
+//app.controller('FAQTotalCandidatesTrainedAssessedCertifiedSchemeWiseController', ['$scope', '$http', function ($scope, $http) {
+//	 //to fill data in scheme drop down
+//	   $scope.schemeDropDownOptions = ['PMKVY','Non-PMKVY'];
+//	  var queryParameter = "/getFAQTotalCandidatesTrainedAssessedCertifiedSchemeWise?batchType="+ $scope.selectedScheme;
+//	  $scope.getDataOfScheme = function getDataOfScheme() {
+//		  var faq2;
+////		  var FAQURI2 = "/getFAQCountOfTotalTrainingCentresConductingTraining?batchType=";
+//		  console.log($scope.selectedScheme);
+//		  console.log('/getFAQTotalCandidatesTrainedAssessedCertifiedSchemeWise?batchType='+ $scope.selectedScheme)
+//		  $http.post(queryParameter).then(function(scheme)
+//						{
+//			  
+//					     console.log(angular.fromJson(scheme));
+//					     faq2 = angular.fromJson(scheme);
+//					     $scope.scheme=faq2.totalNoOfCandidatesEnrolled;
+//					     
+//						});
+//		  }
+//	     
+//	}
+//	]);
 
 
-app.controller('FAQScreenController3', ['$scope', '$http', function ($scope, $http) {
+app.controller('FAQTotalTrainingCentresInAStateController',['$scope', '$http', function ($scope, $http) {
 	 //to fill data in scheme drop down
 	   $scope.stateDropDownOptions = ['Andhra Pradesh','Arunachal Pradesh','Assam','Bihar','Chhattisgarh','Delhi','Goa','Gujarat','Haryana','Himachal Pradesh','Jammu & Kashmir','Jharkhand','Karnataka','Kerala','Madhya Pradesh','Maharastra','Manipur','Meghalaya','Mizoram','Nagaland','Odisha','Punjab','Rajasthan','Sikkim','Tamil Nadu','Telangana','Tripura','Uttarakhand','Uttar Pradesh','West Bengal'];
 //	    $scope.deleteRow = function(row) {
@@ -119,7 +99,7 @@ app.controller('FAQScreenController3', ['$scope', '$http', function ($scope, $ht
 		//  console.log('/getFAQTotalTrainingCentresInAState?state='+ $scope.stateDropDownOptions)
 		  $http.post(getTotalTrainingCenterUrl).then(function(state)
 						{
-					     console.log(state);
+					     console.log(state.data);
 					     $scope.state = state.data;
 					      
 					     
@@ -134,5 +114,60 @@ app.controller('FAQScreenController3', ['$scope', '$http', function ($scope, $ht
 
 
 
+app.controller('FAQCountOfTotalTrainingCentresConductingTrainingController', ['$scope', '$http', function ($scope, $http) {
+	   $scope.schemeDropDownOptions = ['PMKVY','Non-PMKVY'];
+	  var queryParameter = $scope.schemeDropDownOptions;
+	  $scope.getDataOfScheme = function getDataOfScheme() {
+		 
+		  var getFAQCountOfTotalTrainingCentresUrl = "/getFAQCountOfTotalTrainingCentresConductingTraining?scheme="+$scope.selectedScheme;
+		  console.log($scope.selectedScheme);
+		  console.log(getFAQCountOfTotalTrainingCentresUrl);
+			  $http.get('/getFAQCountOfTotalTrainingCentresConductingTraining')
+			    .then(function (response) {
+			    	$scope.number=response.data;
+			     
+			    });
+		  }
+	     
+	}
+	]);
 
 
+
+
+//app.controller('FAQCountOfCandidatesAssessmentUpcomingForAMonthController', ['$scope', '$http', function ($scope, $http) {
+//
+//	   $scope.monthDropDownOptions = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+//	  var queryParameter = $scope.monthDropDownOptions;
+//	  $scope.getDataOfMonth = function getDataOfMonth() {
+//		  var getFAQCountOfCandidatesAssessmentUpcomingForAMonthUrl = "/getFAQCountOfCandidatesAssessmentUpcomingForAMonth?month="+$scope.selectedMonth;
+//		  console.log($scope.selectedMonth);
+//		  console.log(getFAQCountOfCandidatesAssessmentUpcomingForAMonthUrl);
+//		  $http.post(getFAQCountOfCandidatesAssessmentUpcomingForAMonthUrl).then(function(month)
+//						{
+//					     console.log(month.data);
+//					     $scope.month = month.data;
+//
+//						});
+//		  }
+//	     
+//	}
+//	]);
+//
+//
+//
+//app.controller('FAQCountTotalNumberOfBatchesInAParticularStateController', ['$scope', '$http', function ($scope, $http) {
+//	   $scope.stateDropDownOptions = ['Andhra Pradesh','Arunachal Pradesh','Assam','Bihar','Chhattisgarh','Delhi','Goa','Gujarat','Haryana','Himachal Pradesh','Jammu & Kashmir','Jharkhand','Karnataka','Kerala','Madhya Pradesh','Maharastra','Manipur','Meghalaya','Mizoram','Nagaland','Odisha','Punjab','Rajasthan','Sikkim','Tamil Nadu','Telangana','Tripura','Uttarakhand','Uttar Pradesh','West Bengal'];
+//	  var queryParameter = $scope.stateDropDownOptions;
+//	  $scope.getDataOfState = function getDataOfState() {
+//		  var getFAQCountTotalNumberOfBatchesInAParticularStateUrl = "/getFAQCountTotalNumberOfBatchesInAParticularState?state="+$scope.selectedState;
+//		  console.log($scope.selectedState);
+//		  console.log(getFAQCountTotalNumberOfBatchesInAParticularStateUrl);
+//		  $http.post(getFAQCountTotalNumberOfBatchesInAParticularStateUrl).then(function(state)
+//						{
+//					     console.log(state.data);
+//ata;
+//
+//						});
+//		  }
+//	 
