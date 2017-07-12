2036f22a584ea0c31d96ae94c151562be2a647e9
PranjalKumarSingh <pranjal@smaltandberyl.com>
2017-07-12 16:28:20 +0530

    Author          : Pranjal Kumar Singh Story Id        : SSDMS-5 (JIRA) Files Modified  : AbApplicationStatus.js, TpApplicationStatus.js Description     : Bugs Fixed

--- skillIndia/server/src/main/resources/static/js/controllers/TpApplicationStatus.js
+++ skillIndia/server/src/main/resources/static/js/controllers/TpApplicationStatus.js
@@ -7,24 +7,25 @@ app.controller('MainCtrl', ['$scope', '$http', '$location', function ($scope, $h
 		  verticalScrollbar:0,
 		  
   columnDefs : [
-    { name: 'applicationID' ,	displayName:'Application ID',	cellClass:'Prostokt-2-kopia-2-copy-10',	headerCellClass:'Application-ID',	width:260,
-	},
-    { name: 'dateOfSubmission',	displayName:'Date of Submission',	cellClass:'Prostokt-2-kopia-2-copy-11',	headerCellClass:'Date-of-Submission',	width:208,
-	},
-    { name: 'submittedby' ,	displayName:'Submitted By',	cellClass:'Prostokt-2-kopia-2-copy-12',	headerCellClass:'Submitted-By',	width:208,
-	},
-    { name: 'status' ,	displayName:'Status',	cellClass:'Prostokt-2-kopia-2-copy-13',	headerCellClass:'Status',	width:185,
-	},
-    { name:'Action',	cellClass: 'Prostokt-2-kopia-2-copy-14',	width:76,	headerCellClass: 'Action-copy',	cellTemplate:'<img src="icon/indexpageIcons/edit1.png" ng-click=grid.appScope.myfunction()>',
-    },
-    { name: 'comment' ,	displayName:'Comment',	cellClass:'Prostokt-2-kopia-2-copy-15',	headerCellClass:'Comment',	width:201,
-	}
+{ name: 'applicationID' ,	displayName:'Application ID',	cellClass:'Prostokt-2-kopia-2-copy-10',	headerCellClass:'Application-ID',
+},
+{ name: 'dateOfSubmission',	displayName:'Date of Submission',	cellClass:'Prostokt-2-kopia-2-copy-11',	headerCellClass:'Date-of-Submission',
+},
+{ name: 'trainingPartnerName' ,	displayName:'Submitted By',	cellClass:'Prostokt-2-kopia-2-copy-12',	headerCellClass:'Submitted-By',
+},
+{ name: 'applicationState' ,	displayName:'Status',	cellClass:'Prostokt-2-kopia-2-copy-13',	headerCellClass:'Status',
+},
+{ name:'Action',	cellClass: 'Prostokt-2-kopia-2-copy-14',	width:76,	headerCellClass: 'Action-copy',	cellTemplate:'<img src="icon/indexpageIcons/edit1.png" ng-click=grid.appScope.myfunction()>',
+},
+{ name: 'comment' ,	displayName:'Comment',	cellClass:'Prostokt-2-kopia-2-copy-15',	headerCellClass:'Comment',
+}
 	]
   };
 $scope.myfunction = function() 
   {  location.href = 'http://www.google.com';
 };
 $http.get('/trainingPartnerApplicationStatus')
+//$http.get('poc/Status.json')
     .success(function (data) {
       $scope.gridOptions.data = data;
     });
