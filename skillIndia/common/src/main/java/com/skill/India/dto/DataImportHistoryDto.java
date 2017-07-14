32e8ea979f0b4126d5c61f138b18f793751b6acc
poojasharma06 <sharmapooja6797@gmail.com>
2017-07-14 18:33:22 +0530

    Author           : Pooja Sharma Files modified   : DataImportHistoryDto

--- skillIndia/common/src/main/java/com/skill/India/dto/DataImportHistoryDto.java
+++ skillIndia/common/src/main/java/com/skill/India/dto/DataImportHistoryDto.java
@@ -7,67 +7,45 @@ import com.skill.India.common.BaseDto;
 public class DataImportHistoryDto extends BaseDto {
 
 	private static final long serialVersionUID = 1L;
-
-	private final Integer SerialNo;
 	
-	private final String CSVName ;
+	private final String csvName ;
 
-	private final String CSVType;
+	private final String csvType;
 
-	private final Date CSV_Upload_Date;
+	private final Date csvUploadDate;
 	
-	private final String CSV_Upload_UserId;
+	private final String csvUploadUserId;
 
-	/**
-	 * @return the serialversionuid
-	 */
+	
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
 
-	/**
-	 * @return the serialNo
-	 */
-	public Integer getSerialNo() {
-		return SerialNo;
-	}
 
-	/**
-	 * @return the cSVName
-	 */
 	public String getCSVName() {
-		return CSVName;
+		return csvName;
 	}
 
-	/**
-	 * @return the cSVType
-	 */
 	public String getCSVType() {
-		return CSVType;
+		return csvType;
 	}
 
-	/**
-	 * @return the cSV_Upload_Date
-	 */
+	
 	public Date getCSV_Upload_Date() {
-		return CSV_Upload_Date;
+		return csvUploadDate;
 	}
 
-	/**
-	 * @return the cSV_Upload_UserId
-	 */
+
 	public String getCSV_Upload_UserId() {
-		return CSV_Upload_UserId;
+		return csvUploadUserId;
 	}
 
-	public DataImportHistoryDto(Integer serialNo, String cSVName,
-			String cSVType, Date cSV_Upload_Date, String cSV_Upload_UserId) {
+	public DataImportHistoryDto(String cSVName,String cSVType, Date cSV_Upload_Date, String cSV_Upload_UserId) {
 		super();
-		SerialNo = serialNo;
-		CSVName = cSVName;
-		CSVType = cSVType;
-		CSV_Upload_Date = cSV_Upload_Date;
-		CSV_Upload_UserId = cSV_Upload_UserId;
+		csvName = cSVName;
+		csvType = cSVType;
+		csvUploadDate =cSV_Upload_Date;
+		csvUploadUserId = cSV_Upload_UserId;
 	}
 	
 	
