7602f8f5464df660070f0cfb17f156a4f8b16489
poojasharma06 <sharmapooja6797@gmail.com>
2017-07-14 18:31:21 +0530

    Author           : Pooja Sharma Files modified   : DataImportController

--- skillIndia/common/src/main/java/com/skill/India/controller/DataImportController.java
+++ skillIndia/common/src/main/java/com/skill/India/controller/DataImportController.java
@@ -1,9 +1,16 @@
 package com.skill.India.controller;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Collection;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.skill.India.service.DataImportServices;
@@ -22,4 +29,30 @@ public class DataImportController{
 	public Collection<DataImportHistoryDto> getdataImportHistorydto(){
 	return dataImportServices.getUpdateHistory();
 }
+	@RequestMapping(value = "/files/{file_name}", method = RequestMethod.GET)
+	public void getFile(@PathVariable("file_name") String fileName, 
+	    HttpServletResponse response) {
+	    try {
+	      // get your file as InputStream
+	     
+	     FileInputStream is = new FileInputStream(new File("D://New folder//trunk//skillIndia//server//src//main//resources//static//sheets.csv"));
+	     //FileInputStream is = new FileInputStream(new File("http://www.colorado.edu/conflict/peace/download/peace.zip"));
+	     
+	     // copy it to response's OutputStream
+	      org.apache.commons.io.IOUtils.copy(is,response.getOutputStream());
+	      response.setContentType("application/x-rar-compressed");
+	      response.setHeader("Content-disposition", "attachment; filename="+ fileName+".csv");
+	      //response.flushBuffer();
+	      System.out.println(response.getOutputStream());
+	      //return response;
+	    } catch (IOException ex) {
+	      //.info("Error writing file to output stream. Filename was '{}'", fileName, ex);
+	      throw new RuntimeException("IOError writing file to output stream");
+	    }
+
+	    
+	    
+	    
+	}
+	
 }
