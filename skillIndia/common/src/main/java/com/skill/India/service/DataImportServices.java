0c303be284f4307c551704012ebf3ae9a701dc68
poojasharma06 <sharmapooja6797@gmail.com>
2017-07-14 18:34:11 +0530

    Author           : Pooja Sharma Files modified   : DataImportServices

--- skillIndia/common/src/main/java/com/skill/India/service/DataImportServices.java
+++ skillIndia/common/src/main/java/com/skill/India/service/DataImportServices.java
@@ -1,10 +1,13 @@
 package com.skill.India.service;
 import java.util.Collection;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.skill.India.dto.DataImportHistoryDto;
+
 import com.skill.India.dao.DataImportHistoryDao;
+
 
 
 
@@ -13,9 +16,17 @@ public class DataImportServices {
 	
 	@Autowired
 	private DataImportHistoryDao dataImportHistoryDao;
-
-	public Collection<DataImportHistoryDto> getUpdateHistory() {
-	   return dataImportHistoryDao.getUpdateHistory();
-	}
-
+	public Collection<DataImportHistoryDto> getUpdateHistory(){
+//	Collection<DataImportHistoryDto> dtoobj=dataImportHistoryDao.getUpdateHistory();
+//	String Path=null;
+//	 for(DataImportHistoryDto x:dtoobj)
+//	  
+//	 {
+//	  String FileName=x.getCSVName();
+//	  Path = ("C:\\Users\\Rachit goyal\\Desktop\\").concat(FileName);
+//	 }
+	 return dataImportHistoryDao.getUpdateHistory();
+//	 return (Path);
+		}
 }
+
