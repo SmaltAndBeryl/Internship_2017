7c3323237075dcedace3afa70e28d565fa0d3f73
PranjalKumarSingh <pranjal@smaltandberyl.com>
2017-07-16 17:21:50 +0530

    Author        : Pranjal Kumar Singh Story Id      : SSDMS 5 (JIRA) File Added    : TrainingPartnerStatusController.java Description   : Controller For Training Partner Home page.

---
+++ skillIndia/common/src/main/java/com/skill/India/POC/TrainingPartnerStatusController.java
@@ -0,0 +1,13 @@
+package com.skill.India.POC;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TpStatusController {
+
+	@RequestMapping("/trainingPartnerHomePage")  
+	  public  String user() {
+	    return "MasterTemplate2Tp";
+	  }
+}
