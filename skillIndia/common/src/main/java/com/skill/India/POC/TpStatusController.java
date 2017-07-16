688d899e16429c7be662d06d957e2ee0242e5e6e
PranjalKumarSingh <pranjal@smaltandberyl.com>
2017-07-16 17:51:21 +0530

    Author        : Pranjal Kumar Singh Story Id      : SSDMS 5 (JIRA) File Added    : TPStatusController.java Files Deleted : TrainingPartnerStatusController.java Description   : Controller for Training Partner Application Status

--- skillIndia/common/src/main/java/com/skill/India/POC/TpStatusController.java
+++ skillIndia/common/src/main/java/com/skill/India/POC/TpStatusController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class TpStatusController {
+public class TPStatusController {
 
-	@RequestMapping("/trainingPartnerHomePage")  
+	@RequestMapping("/tPHomePage")  
 	  public  String user() {
-	    return "MasterTemplate2";
+	    return "MasterTemplate2Tp";
 	  }
 }
