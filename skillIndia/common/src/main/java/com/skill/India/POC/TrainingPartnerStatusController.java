89bd729925e8f43fa9f8969e5a705976bb08c708
PranjalKumarSingh <pranjal@smaltandberyl.com>
2017-07-16 17:23:48 +0530

    Author        : Pranjal Kumar Singh Story Id      : SSDMS 5 (JIRA) File Added    : TrainingPartnerStatusController.java Description   : Bugs Fixed

--- skillIndia/common/src/main/java/com/skill/India/POC/TrainingPartnerStatusController.java
+++ skillIndia/common/src/main/java/com/skill/India/POC/TrainingPartnerStatusController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class TpStatusController {
+public class TrainingPartnerStatusController {
 
 	@RequestMapping("/trainingPartnerHomePage")  
 	  public  String user() {
