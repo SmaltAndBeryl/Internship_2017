b3a6d23129eea4f500f15f7348fe9472cf50b066
prateek-kapoor323 <prateek@smaltandberyl.com>
2017-07-16 21:29:12 +0530

    Author        :Prateek Kapoor File Added    : trainingPartnerHomepage.js Description   : Bug fixes

--- skillIndia/server/src/main/resources/static/js/controllers/trainingPartnerHomepage.js
+++ skillIndia/server/src/main/resources/static/js/controllers/trainingPartnerHomepage.js
@@ -1,7 +1,6 @@
 var tpApp = angular
                    .module('tpApp', [ 
                             'ui.grid', 
-                            'ui.grid.edit', 
                             'ui.grid.cellNav',
                             'ui.grid.autoResize'
                             ]);
@@ -12,49 +11,47 @@ var tpApp = angular
 function presentController($scope, $http){
 	    
 		$scope.presentGridOptions={
-				enableSorting: false,
-				enableGridMenus:false, 
-				enableSorting: false, 
-			    enableFiltering: false,
-			    enableCellEdit : false,
-			    enableColumnMenus : false,
-			    verticalScrollbar:0,
-			    
-		columnDefs:[
-		{name:'batchId',displayname:'Batch ID', cellClass:'bId', headerCellClass:'batchId' },
-		{name:'completionDate', displayname:'Completion Date', cellClass:'cDate',  headerCellClass:'compDate'},
-		{name:'instructorName', displayname:'Instructor Name', cellClass:'iName', headerCellClass:'insName'},
-		{name:'noOfCandidates', displayname:'Number of Candidates', cellClass:'noCandidates', headerCellClass:'candidates'},
-				]			 		            			            	
-	};
+					  enableColumnMenus: false,
+						  enableSorting: false,
+						  verticalScrollbar:0,
+						  
+				  columnDefs : [
+				{ name: 'batchId' ,	displayName:'Batch ID',	cellClass:'Prostokt-2-kopia-2-copy-10',	headerCellClass:'bId',
+				},
+				{ name: 'completionDate',	displayName:'Completion Date',	cellClass:'Prostokt-2-kopia-2-copy-11',	headerCellClass:'compDate',
+				},
+				{ name: 'instructorName' ,	displayName:'Instructor Name',	cellClass:'Prostokt-2-kopia-2-copy-12',	headerCellClass:'insName',
+				},
+				{ name: 'noOfCandidates' ,	displayName:'Number of Candidates',	cellClass:'Prostokt-2-kopia-2-copy-13',	headerCellClass:'candidates',
+				}
+					]
+			};	 		            			            
 		
 		 $http.get("/trainingPartnerPastBatches")
 		    .then(function (response) {
 		      $scope.presentGridOptions.data= response.data.PresentBatches;
 		    })
-		    
-		    
-		    $scope.pastGridOptions={
-				enableSorting: false,
-				enableGridMenus:false,  
-				enableSorting: false, 
-			    enableFiltering: false,
-			    enableCellEdit : false,
-			    enableColumnMenus : false,
-			    verticalScrollbar:0,
-			    
-		columnDefs:[
-		{name:'batchId',displayname:'Batch ID', cellClass:'bId',  headerCellClass:'batchId' },
-		{name:'completionDate', displayname:'Completion Date', cellClass:'cDate',  headerCellClass:'compDate'},
-		{name:'instructorName', displayname:'Instructor Name', cellClass:'iName', headerCellClass:'insName'},
-		{name:'noOfCandidates', displayname:'Number of Candidates', cellClass:'noCandidates', headerCellClass:'candidates'},
-		{name:'Display', displayName: 'Download Certificate', cellClass:'download',  headerCellClass:'downCer', cellTemplate: '<img src="icon/CertificateDownlad/Certificate Download.png" ng-click=grid.appScope.myfunction()>'}
-		]			 		            			            	
-	
-		};
+		
+		$scope.pastGridOptions={
+					  enableColumnMenus: false,
+						  enableSorting: false,
+						  verticalScrollbar:0,	    
+						  columnDefs : [
+										{ name: 'batchId' ,	displayName:'Batch ID',	cellClass:'Prostokt-2-kopia-2-copy-10',	headerCellClass:'bId',width: 100
+										},
+										{ name: 'completionDate',	displayName:'Completion Date',	cellClass:'Prostokt-2-kopia-2-copy-11',	headerCellClass:'compDate', width: 150
+										},
+										{ name: 'instructorName' ,	displayName:'Instructor Name',	cellClass:'Prostokt-2-kopia-2-copy-12',	headerCellClass:'insName',width: 150
+										},
+										{ name: 'noOfCandidates' ,	displayName:'Number of Candidates',	cellClass:'Prostokt-2-kopia-2-copy-13',	headerCellClass:'candidates',
+										},
+										{name:'Display', displayName: 'Download Certificate', cellClass:'download',  headerCellClass:'downCer', cellTemplate: '<img src="icon/CertificateDownlad/Certificate Download.png" ng-click=grid.appScope.myfunction()>'}
+											]	
+		 };
 		 $http.get("/trainingPartnerPastBatches")
 		    .then(function (response) {
 		      $scope.pastGridOptions.data= response.data.PastBatches;
-		    })
-		    
-	};
+		    })	
+};
+	
+	
