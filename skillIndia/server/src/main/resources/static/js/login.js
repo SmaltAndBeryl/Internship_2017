8f572f87848699ebba27d915e53397255dea79fe
bhaskarmamgai <bhaskar90196@gmail.com>
2017-07-17 22:36:03 +0530

    Author     : Bhaskar File modified : login.js Description   : Code refactoring

--- skillIndia/server/src/main/resources/static/js/login.js
+++ skillIndia/server/src/main/resources/static/js/login.js
@@ -35,17 +35,17 @@ document.getElementById('toggleBt').onclick = function() {
 
 console.log("File reached");
 
-//    $(function() {
-//
-//    $('#login-form-link').click(function(e) {
-//		$("#login-form").delay(100).fadeIn(100);
-// 		$("#register-form").fadeOut(100);
-//		$('#register-form-link').removeClass('active');
-//		$(this).addClass('active');
-//		e.preventDefault();
-//		$('#password').password('toggle');
-//
-//	});
+    $(function() {
+
+    $('#login-form-link').click(function(e) {
+		$("#login-form").delay(100).fadeIn(100);
+ 		$("#register-form").fadeOut(100);
+		$('#register-form-link').removeClass('active');
+		$(this).addClass('active');
+		e.preventDefault();
+		$('#password').password('toggle');
+
+	});
 	$('#register-form-link').click(function(e) {
 		$("#register-form").delay(100).fadeIn(100);
  		$("#login-form").fadeOut(100);
@@ -54,7 +54,7 @@ console.log("File reached");
 		e.preventDefault();
 	});
 
-//});
+});
 
         
     
