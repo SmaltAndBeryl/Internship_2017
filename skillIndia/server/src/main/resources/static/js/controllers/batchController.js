881620daefa9a3022348021ef81346aed20fd799
Alkesh <alkeshsrivastava11@gmail.com>
2017-07-17 10:19:38 +0530

    Author          : Alkesh Srivastava Task            : Integrate the remaining tables of the batch assignment page Files Modified  : batchController.js, batch.html, nonAssignedBatches.yml, pdfData.yml Files Deleted   : none Description     : Integrated the remaining tables in the master template(minor back-end issues, major CSS flaws). Also modified the SQL syntax in accordance with the slightly changed DB schema.

--- skillIndia/server/src/main/resources/static/js/controllers/batchController.js
+++ skillIndia/server/src/main/resources/static/js/controllers/batchController.js
@@ -11,10 +11,9 @@ var batch = angular
 
 batch.controller('batchController', batchController)
 
-batchController.$inject = ['$scope', '$http'];
+batchController.$inject = ['$scope', '$http', '$log', '$location'];
 
-function batchController($scope, $http){
-
+function batchController($scope, $http, $log, $location){
 
 //    console.log(names);
 
@@ -73,21 +72,209 @@ $scope.show = function(){['Lancelot', 'Touchstone'];}
                 console.log("Status changed to proposed.! "+ btc)
             })
     };
+
+
+
+    //Other tables JS code
+
+    $scope.proposedBatchesBatchAssignmentGridOptions = {
+    		  enableGridMenus: false,
+    		  enableSorting: false,
+    		  enableColumnMenus: false,
+    		  enableFiltering: false,
+    		  enableCellEdit: false,
+
+    	columnDefs : [
+    	{name:'serialNumber',
+    	displayName: 'S.No.',
+    	//cellClass: '',
+    	//headerCellClass: '',
+    	width:73},
+        {name:'batchId',
+        displayName: 'Batch ID',
+        //cellClass: '',
+        //headerCellClass: '',
+        width:100},
+        {name:'location',
+        displayName: 'Location',
+        //cellClass: '',
+        //headerCellClass: '',
+        width:150},
+        {name:'batchEndDate',
+        displayName: 'Batch End Date',
+        //cellClass: '',
+        //headerCellClass: '',
+        width: 140},
+        {name:'assessmentDate',
+        displayName: 'Assessment Date',
+        //cellClass: '',
+        //headerCellClass: '',
+        width:150},
+        {name:'proposedAB',
+        displayName: 'Proposed AB',
+        //cellClass: '',
+        //headerCellClass: '',
+        width:120},
+        {name:'propose',
+         displayName: 'Propose',
+         //cellClass: '',
+         //headerCellClass: '',
+         cellTemplate: '<label><img src="icon/indexPageIcons/close.png" ng-click=grid.appScope.saveFunction(row)></label>',
+         width:155}
+      ]
+    };
+    	  $scope.approvedBatchesBatchAssignmentGridOptions = {
+    			  enableGridMenus: false,
+    			  enableSorting: false,
+    			  enableColumnMenus: false,
+    			  enableFiltering: false,
+    			  enableCellEdit: false,
+
+    	columnDefs : [
+        { name:'serialNumber',
+        displayName: 'S.No.',
+        //cellClass: '',
+        //headerCellClass: '',
+        width:73},
+        {name:'batchId',
+        displayName: 'Batch ID',
+        //cellClass: '',
+        //headerCellClass: '',
+        width:100},
+        {name:'location',
+        displayName: 'Location',
+        //cellClass: '',
+        //headerCellClass: '',
+        width:150},
+        {name:'batchEndDate',
+        displayName: 'Batch End Date',
+        //cellClass: '',
+        //headerCellClass: '',
+        width: 140},
+        {name:'assessmentDate',
+        displayName: 'Assessment Date',
+        //cellClass: '',
+        //headerCellClass: '',
+        width:150},
+        {name:'proposedAB',
+        displayName: 'Assigned AB',
+        //cellClass: '',
+        //headerCellClass: '',
+        width:150},
+        { name:'withdraw',
+        displayName: 'Withdraw',
+        //cellClass: '',
+        //headerCellClass: '',
+        cellTemplate: '<label><img src="icon/indexPageIcons/close.png" ng-click=grid.appScope.saveFunction(row)></label>',
+        width:155}
+      ]
+    };
+    	 $scope.rejectedBatchesBatchAssignmentGridOptions = {
+    			  enableGridMenus: false,
+    			  enableSorting: false,
+    			  enableColumnMenus: false,
+    			  enableFiltering: false,
+    			  enableCellEdit: false,
+
+    	columnDefs : [
+        { name:'serialNumber',
+        displayName: 'S.No.',
+        //cellClass: '',
+        //headerCellClass: '',
+        width:73},
+        {name:'batchId',
+        displayName: 'Batch ID',
+        //cellClass: '',
+        //headerCellClass: '',
+        width:100},
+        {name:'location',
+        displayName: 'Location',
+        //cellClass: '',
+        //headerCellClass: '',
+        width:150},
+        {name:'batchEndDate',
+        displayName: 'Batch End Date',
+        //cellClass: '',
+        //headerCellClass: '',
+        width: 140},
+        {name:'assessmentDate',
+        displayName: 'Assessment Date',
+        //cellClass: '',
+        //headerCellClass: '',
+        width:150},
+        {name:'proposedAB',
+        displayName: 'Rejected AB',
+        //cellClass: '',
+        //headerCellClass: '',
+        width:150}
+      ]
+    };
+    	 $http.get('/getProposedBatchesBatchAssignment')
+    	 .then(function(response) {
+    	 $scope.proposedBatchesBatchAssignmentGridOptions.data = response.data;
+    	});
+
+    	 $http.get('/getApprovedBatchesBatchAssignment')
+    	 .then(function(response) {
+    	 $scope.approvedBatchesBatchAssignmentGridOptions.data = response.data;
+    	});
+
+    	 $http.get('/getRejectedBatchesBatchAssignment')
+    	 .then(function(response) {
+         $scope.rejectedBatchesBatchAssignmentGridOptions.data = response.data;
+        });
+
+    	    $scope.saveFunction = function(rowData){
+    	        alert("You have withdrawn the assignment");
+
+    	        //Extract first cell value
+    	        var abj = Object.values(Object.values(rowData)[1])[0];
+    	        console.log("Row Data is " + abj);
+
+
+    	        //Code for query update
+    	        var urldata = "/withdrawAssignment";
+
+    	        $http({
+    	            url : urldata,
+    	            method : "POST",
+    	            params : { batchId : abj }
+    	        })
+    	            .then(function(response){
+    	                console.log("Status changed to Default! "+ btc)
+    	            });
+    	    }
+
+    // JS for search bar
+    $scope.gridOptionsSearch = { enableGridMenus : false,
+    		  enableSorting: false,
+    		  enableFiltering: false,
+    		  enableCellEdit : false,
+    		  enableColumnMenus : false,
+    		  enableHideColoumn: false,
+
+      columnDefs : [
+
+        { name:'#' , displayname:'1' , height:'71' , width:'100'},
+        { name:'batchId' , displayname:'Batch id' , cellClass:'batch id' , width:'162' , height:'71' },
+        { name:'state' , displayname:'location' , width:'162' , height:'71' },
+        { name:'batchEndDate' , displayname:'Batch End date' ,  width:'163' , height:'71'},
+        { name:'assessmentDate' , dispalyname:'Assessment Date' , width:'162' , height:'71'},
+        { name:'assessorName' ,displayname:'Proposed AB',  width:'163',height:'71'},
+
+      ]
+    };
+
+    	$scope.getDataOfBatch = function()
+    	{
+    		$http.post('/getInformationOfTheBatchId?batchId='+$scope.batchAssignmentSearch)
+    		  .then(function (response) {
+    			  console.log("inside request method");
+    		    $scope.gridOptionsSearch.data = response.data;
+    		  });
+    	};
+
+
+
 }
 
-//batch.filter('mapGender', function(){
-//                var genderHash = {
-//                    1 : 'male',
-//                    2 : 'female'
-//                };
-//
-//                return function(input){
-//                    if(!input){
-//                    return '';
-//                    }
-//
-//                    else{
-//                        genderHash[input]
-//                    }
-//                }
-//            });
