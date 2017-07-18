622f4e136f9a51d5f1a64f8a789f4e3d65a16ab5
PranjalKumarSingh <pranjal@smaltandberyl.com>
2017-07-18 20:47:47 +0530

    Author        : Pranjal Kumar Singh Story Id      : SSDMS 5 (JIRA) File Deleted  : AssessmentBodyHomepageDto.java Description   : Dto for Assessment Body Homepage.

---
+++ skillIndia/common/src/main/java/com/skill/India/dto/AssessmentBodyHomepageDto.java
@@ -0,0 +1,95 @@
+package com.skill.India.dto;
+
+
+public class AssessmentBodyHomepageDto {
+
+	private Integer batchId;
+	
+	private String trainingPartnerDetails;
+	
+	private String assessmentDate;
+	
+	private String location;
+	
+	private Integer totalCandidatesInBatch;
+
+	/**
+	 * @return the batchId
+	 */
+	public Integer getBatchId() {
+		return batchId;
+	}
+
+	/**
+	 * @param batchId the batchId to set
+	 */
+	public void setBatchId(Integer batchId) {
+		this.batchId = batchId;
+	}
+
+	/**
+	 * @return the trainingPartnerDetails
+	 */
+	public String getTrainingPartnerDetails() {
+		return trainingPartnerDetails;
+	}
+
+	/**
+	 * @param trainingPartnerDetails the trainingPartnerDetails to set
+	 */
+	public void setTrainingPartnerDetails(String trainingPartnerDetails) {
+		this.trainingPartnerDetails = trainingPartnerDetails;
+	}
+
+	/**
+	 * @return the assessmentDate
+	 */
+	public String getAssessmentDate() {
+		return assessmentDate;
+	}
+
+	/**
+	 * @param assessmentDate the assessmentDate to set
+	 */
+	public void setAssessmentDate(String assessmentDate) {
+		this.assessmentDate = assessmentDate;
+	}
+
+	/**
+	 * @return the location
+	 */
+	public String getLocation() {
+		return location;
+	}
+
+	/**
+	 * @param location the location to set
+	 */
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	/**
+	 * @return the totalCandidatesInBatch
+	 */
+	public Integer getTotalCandidatesInBatch() {
+		return totalCandidatesInBatch;
+	}
+
+	/**
+	 * @param totalCandidatesInBatch the totalCandidatesInBatch to set
+	 */
+	public void setTotalCandidatesInBatch(Integer totalCandidatesInBatch) {
+		this.totalCandidatesInBatch = totalCandidatesInBatch;
+	}
+	
+	public AssessmentBodyHomepageDto(Integer batchId, String trainingPartnerDetails, String assessmentDate, String location, Integer totalCandidatesInBatch ){
+		super();
+		this.batchId = batchId;
+		this.trainingPartnerDetails = trainingPartnerDetails;
+		this.assessmentDate = assessmentDate;
+		this.location = location;
+		this.totalCandidatesInBatch = totalCandidatesInBatch;
+	}
+	
+}
