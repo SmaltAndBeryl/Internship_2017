c7888bf6e969c98bfdcf869d047c4096cc5210aa
pushkarshukla0 <pushkarachi@gmail.com>
2017-07-18 17:39:14 +0530

    Author         : Deepak kandpal files added    :ManageRegistrationActionDto.java, MessageDto.java files deleted : manageRegistrationActionDto.java, ManageRegistrationDto.java Description    : Dto files for manage registration page

---
+++ skillIndia/common/src/main/java/com/skill/India/dto/MessageDto.java
@@ -0,0 +1,31 @@
+package com.skill.India.dto;
+
+public class MessageDto {
+	public String successMessage;
+	public String errorMessage;
+	/**
+	 * @return the successMessage
+	 */
+	public String getSuccessMessage() {
+		return successMessage;
+	}
+	/**
+	 * @param successMessage the successMessage to set
+	 */
+	public void setSuccessMessage(String successMessage) {
+		this.successMessage = successMessage;
+	}
+	/**
+	 * @return the errorMessage
+	 */
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+	/**
+	 * @param errorMessage the errorMessage to set
+	 */
+	public void setErrorMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
+
+}
