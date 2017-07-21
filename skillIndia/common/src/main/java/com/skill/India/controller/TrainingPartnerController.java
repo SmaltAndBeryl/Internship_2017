135c394baacaa29df6661636133d82d4c004e22b
shubham6929 <shubham@smaltandberyl.com>
2017-07-21 12:38:09 +0530

    Author               :  Shubham jain Files Modified       :  TrainingPartnerController.java, TrainingPartnerStatusController.java Description          :  Implemented Sessions, Getting UserId and ApplicationId through Sessions.

--- skillIndia/common/src/main/java/com/skill/India/controller/TrainingPartnerController.java
+++ skillIndia/common/src/main/java/com/skill/India/controller/TrainingPartnerController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.skill.India.common.UserIdtoApplicationIdUtility;
+import com.skill.India.dto.SessionManagementDto;
 import com.skill.India.dto.TrainingPartnerBatchesDto;
 import com.skill.India.service.TrainingPartnerBatchesService;
 
@@ -24,10 +26,25 @@ public class TrainingPartnerController {
 	@Autowired
 	private TrainingPartnerBatchesService trainingPartnerPastTableService;
 	
+	@Autowired
+	private UserIdtoApplicationIdUtility userIdtoApplicationIdUtility;
+	
+	@Autowired
+	private SessionManagementDto sessionManagementDto;
+
+	String userId=sessionManagementDto.getUsername();
+	
+	int applicationId = userIdtoApplicationIdUtility.getApplicationId(userId); 
+	
+//	@RequestMapping("/trainingPartnerPastBatches")
+//	public HashMap<String, ArrayList<TrainingPartnerBatchesDto>> getTrainingPartnerPastTableDto (@RequestParam("applicationId")int applicationId) {
+//		
+//			return trainingPartnerPastTableService.getUpdateRowMapper(applicationId);
+//	}
+
 	@RequestMapping("/trainingPartnerPastBatches")
-	public HashMap<String, ArrayList<TrainingPartnerBatchesDto>> getTrainingPartnerPastTableDto (@RequestParam("applicationId")int applicationId) {
+	public HashMap<String, ArrayList<TrainingPartnerBatchesDto>> getTrainingPartnerPastTableDto () {
 		
 			return trainingPartnerPastTableService.getUpdateRowMapper(applicationId);
 	}
-	
 }
--- skillIndia/common/src/main/java/com/skill/India/controller/TrainingPartnerStatusController.java
+++ skillIndia/common/src/main/java/com/skill/India/controller/TrainingPartnerStatusController.java
@@ -5,23 +5,44 @@ import java.util.Collection;
 
 
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.skill.India.common.UserIdtoApplicationIdUtility;
+import com.skill.India.dto.SessionManagementDto;
 import com.skill.India.dto.TrainingPartnerApplicationStatusDto;
 import com.skill.India.service.TrainingPartnerApplicationStatusService;
+import com.skill.India.service.TrainingPartnerBatchesService;
 
 @RestController
 public class TrainingPartnerStatusController
 {
 	@Autowired
 	private TrainingPartnerApplicationStatusService trainingPartnerApplicationStatusService;
-			
+	
+	@Autowired
+	private TrainingPartnerBatchesService trainingPartnerPastTableService;
+	
+	@Autowired
+	private UserIdtoApplicationIdUtility userIdtoApplicationIdUtility;
+	
+	@Autowired
+	private SessionManagementDto sessionManagementDto;
+
+	String userId=sessionManagementDto.getUsername();
+	
+	int applicationId = userIdtoApplicationIdUtility.getApplicationId(userId);
+	
+//	@RequestMapping("/trainingPartnerApplicationStatus")
+//	public Collection<TrainingPartnerApplicationStatusDto> gettrainingPartnerApplicationStatusDto (@RequestParam("applicationId")int applicationId) {
+//	       return trainingPartnerApplicationStatusService.getUpdateRowMapper(applicationId);
+//	}
+
 	@RequestMapping("/trainingPartnerApplicationStatus")
-	public Collection<TrainingPartnerApplicationStatusDto> gettrainingPartnerApplicationStatusDto (@RequestParam("applicationId")int applicationId) {
+	public Collection<TrainingPartnerApplicationStatusDto> gettrainingPartnerApplicationStatusDto () {
 	       return trainingPartnerApplicationStatusService.getUpdateRowMapper(applicationId);
 	}
-
 }
