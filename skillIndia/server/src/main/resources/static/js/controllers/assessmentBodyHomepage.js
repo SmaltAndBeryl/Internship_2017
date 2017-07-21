f03b03d9e791b0b9ca7676c7ef1a78bf308b1226
PranjalKumarSingh <pranjal@smaltandberyl.com>
2017-07-21 11:27:05 +0530

    Author         : Pranjal Kumar Singh Story Id       : SSDMS 5 (JIRA) File Modified  : assessmentBodyApplicationStatus.yml Description    : Correction in Assessment Body Application status table.Author         : Pranjal Kumar Singh
    Story Id       : SSDMS 5 (JIRA)
    
    File Modified  : assessmentBodyApplicationStatus.yml, assessmentBodyHomepage.yml, trainingPartnerApplicationStatus.yml, assessmentBodyHomepage.js, assessmentBodyHomepage.html, MasterTemplate2.html,  assessmentBodyApplicationStatus.js
    
    Description    : Updated Frontend files for Assessment Body Homepage & Training Partner Homepage.

--- skillIndia/server/src/main/resources/static/js/controllers/assessmentBodyHomepage.js
+++ skillIndia/server/src/main/resources/static/js/controllers/assessmentBodyHomepage.js
@@ -8,52 +8,50 @@ var abApp = angular
   abApp.controller('abhomepageController', abhomepageController);
   abhomepageController.$inject=['$scope','$http', '$location'];
 
-function abhomepageController($scope, $http,$location){
-     
-	$scope.gridOptions = {
-		 enableColumnMenus: false,
-		  enableSorting: false,
-		  verticalScrollbar:0,
-		  enablehorizontalScrollbar: true,
-		  
-  columnDefs : [
-{ name: 'applicationID' ,	displayName:'Application ID',	cellClass:'commoncell',	headerCellClass:'Application-ID',
-},
-{ name: 'dateOfSubmission',	displayName:'Date of Submission',	cellClass:'commoncell',	headerCellClass:'Date-of-Submission',
-},
-{ name: 'assessmentAgencyName' ,	displayName:'Submitted By',	cellClass:'commoncell',	headerCellClass:'Submitted-By',
-},
-{ name: 'applicationState' ,	displayName:'Status',	cellClass:'statuscell',	headerCellClass:'Status',
-},
-{ name:'Action',	cellClass: 'Prostokt-2-kopia-2-copy-14',	width:76,	headerCellClass: 'Action-copy',	cellTemplate:'<img src="icon/indexpageIcons/edit1.png" ng-show="1" ng-click="grid.appScope.myfunction(row)">',
-},
-{ name: 'comment' ,	displayName:'Comment',	cellClass:'comment',	headerCellClass:'Comment',cellTemplate:'<center ng-click=grid.appScope.comment(row)>Click to View',
-}
-	]
-  };
+function abhomepageController($scope, $http)
+{
+    
+	$scope.abAppStatus = {
+			  enableColumnMenus: false,
+			  enableSorting: false,
+			  verticalScrollbar:0,
+			  
+	  columnDefs : [
+	{ name: 'applicationID' ,	displayName:'Application ID',	cellClass:'commoncell',	headerCellClass:'Application-ID'},
+	{ name: 'dateOfSubmission',	displayName:'Date of Submission',	cellClass:'commoncell',	headerCellClass:'Date-of-Submission'},
+	{ name: 'assessmentAgencyName' ,	displayName:'Submitted By',	cellClass:'commoncell',	headerCellClass:'Submitted-By'},
+	{ name: 'applicationState' ,	displayName:'Status',	cellClass:'statuscell',	headerCellClass:'Status'},
+	{ name:'Action' ,displayName:'Action'	,cellClass: 'Prostokt-2-kopia-2-copy-14',	width:76,	headerCellClass: 'Action-copy',	cellTemplate:'<img src="icon/indexpageIcons/edit1.png" ng-show="1" ng-click="grid.appScope.myfunction(row)">'},
+	{ name: 'comment' ,	displayName:'Comment',	cellClass:'comment',	headerCellClass:'Comment',cellTemplate:'<center ng-click=grid.appScope.myfunction1(row)>Click to View'}
+		]
+	  };
 	
-	$scope.comment = function(rowData)
-	{ var commentCheck=Object.values(Object.values(rowData)[1])[4];
+	$scope.myfunction1 = function(rowData)
+	{
+		var commentCheck=rowData.entity.comment;
 	window.alert(commentCheck);
+
 }
-	
 $scope.myfunction = function(rowData) 
-  {   var appState=Object.values(Object.values(rowData)[1])[3];
-  if(appState=="Incomplete"){
+{   var appState=Object.values(Object.values(rowData)[1])[3];
+if(appState=="Incomplete"){
 	  window.location="https://www.google.co.in";
 	  console.log("Please Edit the Form");
 	  
-  }
-  else{
-	  window.alert("Invalid Access");
-  }
-	console.log("Click is working"+appState);
 }
-$http.get('/assessmentBodyApplicationStatus')
-//$http.get('poc/AbStatus.json')
-   .then(function (response) {
-      $scope.gridOptions.data = response.data;
-    })
+else{
+	  window.alert("Invalid Access");
+}
+	console.log("Click is working"+appState);
+};
+
+$http.get('/assessmentBodyApplicationStatus?applicationId=2')
+  .then(function (response) {
+    $scope.abAppStatus.data = response.data;
+  },
+  function(errorResponse){
+	  console.log("inside error function application status table");
+  })
     
     //Application status Ends
 	
@@ -70,11 +68,14 @@ $http.get('/assessmentBodyApplicationStatus')
 { name:'totalCandidatesInBatch',displayName:'Candidates',displayName:'No. of Candidates',cellClass:'cell',headerCellClass:'CommonCell'},
 		        	  ]
   };
-	$http.get('/getPastBatchesAssessmentBodyHomepage')
+	$http.get("/getPastBatchesAssessmentBodyHomepage?applicationId=2")
 		.then(function (response) {
       $scope.gridOptions1.data = response.data;
+    },
+    function(errorResponse)
+    {
+    	console.log("inside error");
     })
-
 
     //Past Batches table ends
     
@@ -92,50 +93,56 @@ $http.get('/assessmentBodyApplicationStatus')
 ]
 	};
 	  
-	   $http.get('/getUpcomingBatchesAssessmentBodyHomepage')
+	   $http.get("/getUpcomingBatchesAssessmentBodyHomepage?applicationId=2")
 	  .then(function (response) {
       $scope.gridOptions2.data = response.data;
     })
 	    var bool;
 	    $scope.check = function(rowData){
-	    console.log("Code Reached");
 	    bool = document.getElementById("myCheck").checked 
 	      if(bool == true)
 	       {
-	       console.log("getit");
+	       console.log("Interest captured");
 	       }
 	   }
 	   $scope.confirmfinal = function(rowData){
-		     //console.log("Function Reached");
 		     if(bool==true)
 		      {
 		      var interestid = Object.values(Object.values(rowData)[1])[0];
-		         //Code for query update
 		         var urldata = "/showInterest?batchId="+interestid+"&agencyId=3001";
-		console.log("entered");
 		         $http.post(urldata).then(function(response){
 		          
 		                 console.log("batch id for Interest is "+interestid);
-		                 //$scope.response=response.data;
 		             });
 		      
-		      console.log("you are on right track");
+		      console.log("Interest Shown");
 		      }
 	    }
-	   
-	function confirmfunction2()
-	{
-	  console.log("button Code reached");
-	  console.log("Cnf Function reached");
-	  
-	 if (confirm("Do you want to confirm interest?")==true)
-	     txt = "Your Interest is Confirmed!";
-	 else
-	  txt = "Your Interest is Cancel!";   
-	     document.getElementById("demo").innerHTML = txt;
-	}
 		
 	//Upcomming Batch table ends
+	   
+	   
+	   
+	   $scope.gridstatus = {
+				  enableColumnMenus: false,
+	  enableSorting: false,
+	  verticalScrollbar:0,
+	  
+	  columnDefs : [
+{ name:'batchId',displayName:'Batch ID',cellClass:'cell',headerCellClass:'CommonCell'},
+{ name:'trainingPartnerDetails',displayName:'TP Name|Email|Contact No',cellClass:'cell',headerCellClass:'CommonCell'},
+{ name:'assessmentDate',displayName:'Assessment Date',cellClass:'cell',headerCellClass:'CommonCell'},
+{ name:'location',displayName:'District|State',cellClass:'cell',headerCellClass:'CommonCell'},
+{ name:'totalCandidatesInBatch',displayName:'Candidates',displayName:'No. of Candidates',cellClass:'cell',headerCellClass:'CommonCell'},
+	        	  ]
+};
+$http.get("/getShownInterestBatchesAssessmentBodyHomepage?applicationId=2")
+	.then(function (response) {
+$scope.gridstatus.data = response.data;
+})
+
+//Interest status
+
 		$scope.gridOptions3 = {
 				  enableGridMenus : false,  
 				  enableSorting: false, 
@@ -155,30 +162,22 @@ $http.get('/assessmentBodyApplicationStatus')
 		$scope.approve = function(rowData){
 	         //Extract first cell value
 	         var approveid = Object.values(Object.values(rowData)[1])[0];
-
-
-	         //Code for query update
 	         var urldata = "/approveAssignment?batchId="+approveid;
 
 	         $http.post(urldata).then(function(response){
 	                 console.log("batch id for Approve is "+approveid);
-	                 //$scope.response=response.data;
 	             });
 	     }
 		$scope.reject = function(rowData){
 		      var rejectid = Object.values(Object.values(rowData)[1])[0];
-
-
-		         //Code for query update
 		         var urldata = "/rejectAssignment?batchId="+rejectid;
 
 		         $http.post(urldata).then(function(response){
 		                 console.log("batch id for Approve is "+rejectid);
-		                 //$scope.response=response.data;
 		             });
 		     }
 	  
-		$http.get('/getAssignedBatchesAssessmentBodyHomepage')
+		$http.get("/getAssignedBatchesAssessmentBodyHomepage?applicationId=2")
 	    		.then(function (response) {
       $scope.gridOptions3.data = response.data;
 	    })
@@ -198,7 +197,7 @@ columnDefs : [
 { name:'totalCandidatesInBatch',displayName:'Candidates',displayName:'No. of Candidates',cellClass:'cell',headerCellClass:'CommonCell'},
 ]
 };
-	$http.get('/getConfirmedBatchesAssessmentBodyHomepage')
+	$http.get("/getConfirmedBatchesAssessmentBodyHomepage?applicationId=2")
 	.then(function (response) {
       $scope.gridOptions4.data = response.data;
     })
