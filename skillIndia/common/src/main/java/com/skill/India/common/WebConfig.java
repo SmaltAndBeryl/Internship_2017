cbe33b1048e8bdfdc8fab3c583f9001f2c1181ad
achinchawla <achinchawla@gmail.com>
2017-07-22 19:39:40 +0530

    Privilege

--- skillIndia/common/src/main/java/com/skill/India/common/WebConfig.java
+++ skillIndia/common/src/main/java/com/skill/India/common/WebConfig.java
@@ -3,18 +3,30 @@ package com.skill.India.common;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-
-
-
+/**
+ * The Class WebConfig.
+ */
 @Configuration
 public class WebConfig extends WebMvcConfigurerAdapter {
 
       
-  
+
+	/** The interceptor. */
+	@Autowired
+	private PrivilegeInterceptor interceptor;
+	
+	/* (non-Javadoc)
+	 * @see org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter#addInterceptors(org.springframework.web.servlet.config.annotation.InterceptorRegistry)
+	 */
+	@Override
+    public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(interceptor);
+        
+    }
 
  
     /**
@@ -25,12 +37,12 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
 
-        registry.addViewController("/").setViewName("index");
-        registry.addViewController("/endpoints").setViewName("endpoints");
-        registry.addViewController("/home").setViewName("index");
-        registry.addViewController("/logoffUser").setViewName("logoffUser");
-        registry.addViewController("/login").setViewName("login");
-                
+//        registry.addViewController("/").setViewName("index");
+//        registry.addViewController("/endpoints").setViewName("endpoints");
+//        registry.addViewController("/home").setViewName("index");
+//        registry.addViewController("/logoffUser").setViewName("logoffUser");
+//        registry.addViewController("/login").setViewName("login");
+//                
     }
     
     
