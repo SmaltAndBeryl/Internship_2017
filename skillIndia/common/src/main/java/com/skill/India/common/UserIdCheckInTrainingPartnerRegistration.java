5ee085e21e189848e860e47dc0eace9a6a61455e
ruchitjain <ruchit.jain15@gmail.com>
2017-07-28 19:15:41 +0530

    Author : Ruchit Jain Story : Profile Creation Files Added : UserIdCheckInTrainingPartnerRegistration.java Description : Missed uploading UserIdCheckInTrainingPartnerRegistration.java last time.

---
+++ skillIndia/common/src/main/java/com/skill/India/common/UserIdCheckInTrainingPartnerRegistration.java
@@ -0,0 +1,25 @@
+package com.skill.India.common;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.skill.India.dao.ProfileCreationTrainingPartnerDao;
+
+@Service
+public class UserIdCheckInTrainingPartnerRegistration {
+
+	@Autowired
+	private ProfileCreationTrainingPartnerDao profileCreationTrainingPartnerDao;
+	
+	public int userIdCheckInTrainingPartnerRegistration(String userId)
+	{
+		try{
+		return profileCreationTrainingPartnerDao.profileCreationCheckUserIdExistence(userId);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+			return 2;
+		}
+	}
+}
