6b45b66cacd47352e99b8aa11e9483835d35866d
Alkesh <alkeshsrivastava11@gmail.com>
2017-07-30 22:05:34 +0530

    Description : Added JS for AB and TP

---
+++ skillIndia/server/src/main/resources/static/js/assessmentBody.js
@@ -0,0 +1,362 @@
+var assessmentBody = angular.module("hello");
+
+assessmentBody.controller("assessmentBody", function($scope, $location, $http) {
+    $scope.message = "Hi Assessment Body";
+    $scope.abAppStatus = {
+        enableColumnMenus: false,
+        enableSorting: false,
+        verticalScrollbar: 0,
+
+        columnDefs: [{
+                name: 'applicationID',
+                displayName: 'Application ID',
+                cellClass: 'commoncell',
+                headerCellClass: 'Application-ID'
+            },
+            {
+                name: 'dateOfSubmission',
+                displayName: 'Date of Submission',
+                cellClass: 'commoncell',
+                headerCellClass: 'Date-of-Submission'
+            },
+            {
+                name: 'assessmentAgencyName',
+                displayName: 'Submitted By',
+                cellClass: 'commoncell',
+                headerCellClass: 'Submitted-By'
+            },
+            {
+                name: 'applicationState',
+                displayName: 'Status',
+                cellClass: 'statuscell',
+                headerCellClass: 'Status'
+            },
+            {
+                name: 'Action',
+                displayName: 'Action',
+                cellClass: 'Prostokt-2-kopia-2-copy-14',
+                width: 76,
+                headerCellClass: 'Action-copy',
+                cellTemplate: '<img src="icon/indexpageIcons/edit1.png" ng-show="1" ng-click="grid.appScope.myfunction(row)">'
+            },
+            {
+                name: 'comment',
+                displayName: 'Comment',
+                cellClass: 'comment',
+                headerCellClass: 'Comment',
+                cellTemplate: '<center ng-click=grid.appScope.myfunction1(row)>Click to View'
+            }
+        ]
+    };
+
+    $scope.myfunction1 = function(rowData) {
+        var commentCheck = rowData.entity.comment;
+        window.alert(commentCheck);
+
+    }
+
+    $scope.myfunction = function(rowData) {
+        var appState = Object.values(Object.values(rowData)[1])[3];
+        if (appState == "Incomplete") {
+            window.location = "https://www.google.co.in";
+            //$location.path();
+            console.log("Please Edit the Form");
+
+        } else {
+            window.alert("Invalid Access");
+        }
+        console.log("Click is working" + appState);
+    };
+
+    $http.get('/assessmentBodyApplicationStatus')
+        .then(function(response) {
+                $scope.abAppStatus.data = response.data;
+            },
+            function(errorResponse) {
+                console.log("inside error function application status table");
+            })
+
+    //Application status Ends
+
+    $scope.gridOptions1 = {
+        enableColumnMenus: false,
+        enableSorting: false,
+        verticalScrollbar: 0,
+
+        columnDefs: [{
+                name: 'batchId',
+                displayName: 'Batch ID',
+                cellClass: 'cell',
+                headerCellClass: 'CommonCell'
+            },
+            {
+                name: 'trainingPartnerDetails',
+                displayName: 'TP Name|Email|Contact No',
+                cellClass: 'cell',
+                headerCellClass: 'CommonCell'
+            },
+            {
+                name: 'assessmentDate',
+                displayName: 'Assessment Date',
+                cellClass: 'cell',
+                headerCellClass: 'CommonCell'
+            },
+            {
+                name: 'location',
+                displayName: 'District|State',
+                cellClass: 'cell',
+                headerCellClass: 'CommonCell'
+            },
+            {
+                name: 'totalCandidatesInBatch',
+                displayName: 'Candidates',
+                displayName: 'No. of Candidates',
+                cellClass: 'cell',
+                headerCellClass: 'CommonCell'
+            },
+        ]
+    };
+    $http.get("/getPastBatchesAssessmentBodyHomepage")
+        .then(function(response) {
+                $scope.gridOptions1.data = response.data;
+            },
+            function(errorResponse) {
+                console.log("inside error");
+            })
+
+    //Past Batches table ends
+
+    $scope.gridOptions2 = {
+        enableColumnMenus: false,
+        enableSorting: false,
+
+        columnDefs: [{
+                name: 'batchId',
+                displayName: 'Batch ID',
+                cellClass: 'cell',
+                headerCellClass: 'CommonCell'
+            },
+            {
+                name: 'trainingPartnerDetails',
+                displayName: 'TP Name|Email|Contact No',
+                cellClass: 'cell',
+                headerCellClass: 'CommonCell'
+            },
+            {
+                name: 'assessmentDate',
+                displayName: 'Assessment Date',
+                cellClass: 'cell',
+                headerCellClass: 'CommonCell'
+            },
+            {
+                name: 'location',
+                displayName: 'District|State',
+                cellClass: 'cell',
+                headerCellClass: 'CommonCell'
+            },
+            {
+                name: 'totalCandidatesInBatch',
+                displayName: 'Candidates',
+                displayName: 'No. of Candidates',
+                cellClass: 'cell',
+                headerCellClass: 'CommonCell'
+            },
+            {
+                name: 'Show Interest',
+                cellClass: 'check',
+                headerCellClass: 'CommonCell',
+                cellTemplate: '<div><input type="checkbox" id="myCheck" value="abc" ng-click=grid.appScope.check(row)>&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp<img src="icon/indexPageIcons/tick.png" ng-click=grid.appScope.confirmfinal(row)></div>'
+            }
+        ]
+    };
+
+    $http.get("/getUpcomingBatchesAssessmentBodyHomepage")
+        .then(function(response) {
+            $scope.gridOptions2.data = response.data;
+        })
+    var bool;
+    $scope.check = function(rowData) {
+        bool = document.getElementById("myCheck").checked
+        if (bool == true) {
+            console.log("Interest captured");
+        }
+    }
+    $scope.confirmfinal = function(rowData) {
+        if (bool == true) {
+            var interestid = Object.values(Object.values(rowData)[1])[0];
+            var urldata = "/showInterest?batchId=" + interestid + "&agencyId=3001";
+            //remove agency Id- pick it from session
+            //let batch id as is
+            $http.post(urldata).then(function(response) {
+
+                console.log("batch id for Interest is " + interestid);
+            });
+
+            console.log("Interest Shown");
+        }
+    }
+
+    //Upcomming Batch table ends
+
+    $scope.gridstatus = {
+        enableColumnMenus: false,
+        enableSorting: false,
+        verticalScrollbar: 0,
+
+        columnDefs: [{
+                name: 'batchId',
+                displayName: 'Batch ID',
+                cellClass: 'cell',
+                headerCellClass: 'CommonCell'
+            },
+            {
+                name: 'trainingPartnerDetails',
+                displayName: 'TP Name|Email|Contact No',
+                cellClass: 'cell',
+                headerCellClass: 'CommonCell'
+            },
+            {
+                name: 'assessmentDate',
+                displayName: 'Assessment Date',
+                cellClass: 'cell',
+                headerCellClass: 'CommonCell'
+            },
+            {
+                name: 'location',
+                displayName: 'District|State',
+                cellClass: 'cell',
+                headerCellClass: 'CommonCell'
+            },
+            {
+                name: 'totalCandidatesInBatch',
+                displayName: 'Candidates',
+                displayName: 'No. of Candidates',
+                cellClass: 'cell',
+                headerCellClass: 'CommonCell'
+            },
+        ]
+    };
+    $http.get("/getShownInterestBatchesAssessmentBodyHomepage")
+        .then(function(response) {
+            $scope.gridstatus.data = response.data;
+        })
+
+    //Interest status
+
+    $scope.gridOptions3 = {
+        enableGridMenus: false,
+        enableSorting: false,
+        enableFiltering: false,
+        enableCellEdit: false,
+        enableColumnMenus: false,
+
+        columnDefs: [{
+                name: 'batchId',
+                displayName: 'Batch ID',
+                cellClass: 'cell',
+                headerCellClass: 'CommonCell'
+            },
+            {
+                name: 'trainingPartnerDetails',
+                displayName: 'TP Name|Email|Contact No',
+                cellClass: 'cell',
+                headerCellClass: 'CommonCell'
+            },
+            {
+                name: 'assessmentDate',
+                displayName: 'Assessment Date',
+                cellClass: 'cell',
+                headerCellClass: 'CommonCell'
+            },
+            {
+                name: 'location',
+                displayName: 'District|State',
+                cellClass: 'cell',
+                headerCellClass: 'CommonCell'
+            },
+            {
+                name: 'totalCandidatesInBatch',
+                displayName: 'Candidates',
+                displayName: 'No. of Candidates',
+                cellClass: 'cell',
+                headerCellClass: 'CommonCell'
+            },
+            {
+                name: 'Action',
+                cellClass: 'check',
+                headerCellClass: 'CommonCell',
+                cellTemplate: '<label><img src="icon/indexPageIcons/tick.png" ng-click=grid.appScope.approve(row)>  &nbsp; &nbsp; &nbsp; &nbsp; <img src="icon/indexPageIcons/close.png" ng-click=grid.appScope.reject(row)></label>'
+            }
+        ]
+    };
+
+    $scope.approve = function(rowData) {
+        //Extract first cell value
+        var approveid = rowData.entity.batchId;
+        var urldata = "/approveAssignment?batchId=" + approveid;
+
+        $http.post(urldata).then(function(response) {
+            console.log("batch id for Approve is " + approveid);
+        });
+    }
+    $scope.reject = function(rowData) {
+        var rejectid = rowData.entity.batchId;
+        var urldata = "/rejectAssignment?batchId=" + rejectid;
+
+        $http.post(urldata).then(function(response) {
+            console.log("batch id for Approve is " + rejectid);
+        });
+    }
+
+    $http.get("/getAssignedBatchesAssessmentBodyHomepage")
+        .then(function(response) {
+            $scope.gridOptions3.data = response.data;
+        })
+
+    //Assign Batch table ends
+    $scope.gridOptions4 = {
+        enableColumnMenus: false,
+        enableSorting: false,
+        verticalScrollbar: 0,
+
+
+        columnDefs: [{
+                name: 'batchId',
+                displayName: 'Batch ID',
+                cellClass: 'cell',
+                headerCellClass: 'CommonCell'
+            },
+            {
+                name: 'trainingPartnerDetails',
+                displayName: 'TP Name|Email|Contact No',
+                cellClass: 'cell',
+                headerCellClass: 'CommonCell'
+            },
+            {
+                name: 'assessmentDate',
+                displayName: 'Assessment Date',
+                cellClass: 'cell',
+                headerCellClass: 'CommonCell'
+            },
+            {
+                name: 'location',
+                displayName: 'District|State',
+                cellClass: 'cell',
+                headerCellClass: 'CommonCell'
+            },
+            {
+                name: 'totalCandidatesInBatch',
+                displayName: 'Candidates',
+                displayName: 'No. of Candidates',
+                cellClass: 'cell',
+                headerCellClass: 'CommonCell'
+            },
+        ]
+    };
+    $http.get("/getConfirmedBatchesAssessmentBodyHomepage")
+        .then(function(response) {
+            $scope.gridOptions4.data = response.data;
+        })
+
+    //Confirmed Batch table ends
+});
---
+++ skillIndia/server/src/main/resources/static/js/trainingPartner.js
@@ -0,0 +1,168 @@
+var trainingPartner = angular.module("hello");
+
+trainingPartner.controller("trainingPartner" , function($scope, $http){
+    $scope.message = "Hi Training Partner";
+    $scope.tpAppStatus = {
+        enableColumnMenus: false,
+        enableSorting: false,
+        verticalScrollbar: 0,
+
+        columnDefs: [{
+                name: 'applicationID',
+                displayName: 'Application ID',
+                cellClass: 'commoncell',
+                headerCellClass: 'Application-ID'
+            },
+            {
+                name: 'dateOfSubmission',
+                displayName: 'Date of Submission',
+                cellClass: 'commoncell',
+                headerCellClass: 'Date-of-Submission'
+            },
+            {
+                name: 'trainingPartnerName',
+                displayName: 'Submitted By',
+                cellClass: 'commoncell',
+                headerCellClass: 'Submitted-By'
+            },
+            {
+                name: 'applicationState',
+                displayName: 'Status',
+                cellClass: 'statuscell',
+                headerCellClass: 'Status'
+            },
+            {
+                name: 'Action',
+                displayName: 'Action',
+                cellClass: 'Prostokt-2-kopia-2-copy-14',
+                width: 76,
+                headerCellClass: 'Action-copy',
+                cellTemplate: '<img src="icon/indexpageIcons/edit1.png" ng-show="1" ng-click="grid.appScope.myfunction(row)">'
+            },
+            {
+                name: 'comment',
+                displayName: 'Comment',
+                cellClass: 'comment',
+                headerCellClass: 'Comment',
+                cellTemplate: '<center ng-click=grid.appScope.myfunction1(row)>Click to View'
+            }
+        ]
+    };
+
+    $scope.myfunction1 = function(rowData) {
+        var commentCheck = rowData.entity.comment;
+        window.alert(commentCheck);
+
+    }
+    $scope.myfunction = function(rowData) {
+        var appState = Object.values(Object.values(rowData)[1])[3];
+        if (appState == "Incomplete") {
+            window.location = "https://setprofilecreation";
+            console.log("Please Edit the Form");
+
+        } else {
+            window.alert("Invalid Access");
+        }
+        console.log("Click is working" + appState);
+    };
+
+    $http.get('/trainingPartnerApplicationStatus')
+        .then(function(response) {
+                $scope.tpAppStatus.data = response.data;
+            },
+            function(errorResponse) {
+                console.log("inside error function application status table");
+            })
+    //tp status ends
+
+    $scope.presentGridOptions = {
+        enableColumnMenus: false,
+        enableSorting: false,
+        verticalScrollbar: 0,
+
+        columnDefs: [{
+                name: 'batchId',
+                displayName: 'Batch ID',
+                cellClass: 'Prostokt-2-kopia-2-copy-10',
+                headerCellClass: 'bId',
+            },
+            {
+                name: 'completionDate',
+                displayName: 'Completion Date',
+                cellClass: 'Prostokt-2-kopia-2-copy-11',
+                headerCellClass: 'compDate',
+            },
+            {
+                name: 'instructorName',
+                displayName: 'Instructor Name',
+                cellClass: 'Prostokt-2-kopia-2-copy-12',
+                headerCellClass: 'insName',
+            },
+            {
+                name: 'noOfCandidates',
+                displayName: 'Number of Candidates',
+                cellClass: 'Prostokt-2-kopia-2-copy-13',
+                headerCellClass: 'candidates',
+            }
+        ]
+    };
+
+    $http.get("/trainingPartnerPastBatches")
+        .then(function(response) {
+            $scope.presentGridOptions.data = response.data.PresentBatches;
+        })
+
+    $scope.pastGridOptions = {
+        enableColumnMenus: false,
+        enableSorting: false,
+        verticalScrollbar: 0,
+        columnDefs: [{
+                name: 'batchId',
+                displayName: 'Batch ID',
+                cellClass: 'Prostokt-2-kopia-2-copy-10',
+                headerCellClass: 'bId',
+                width: 100
+            },
+            {
+                name: 'completionDate',
+                displayName: 'Completion Date',
+                cellClass: 'Prostokt-2-kopia-2-copy-11',
+                headerCellClass: 'compDate',
+                width: 150
+            },
+            {
+                name: 'instructorName',
+                displayName: 'Instructor Name',
+                cellClass: 'Prostokt-2-kopia-2-copy-12',
+                headerCellClass: 'insName',
+                width: 150
+            },
+            {
+                name: 'noOfCandidates',
+                displayName: 'Number of Candidates',
+                cellClass: 'Prostokt-2-kopia-2-copy-13',
+                headerCellClass: 'candidates',
+            },
+            {
+                name: 'Display',
+                displayName: 'Download Certificate',
+                cellClass: 'download',
+                headerCellClass: 'downCer',
+                cellTemplate: '<img src="icon/CertificateDownlad/Certificate Download.png" ng-click=grid.appScope.downloadCertificate(row)>'
+            }
+        ]
+    };
+    $http.get("/trainingPartnerPastBatches")
+        .then(function(response) {
+            $scope.pastGridOptions.data = response.data.PastBatches;
+        })
+
+
+    $scope.downloadCertificate = function(rowData) {
+        var fileName = rowData.entity.batchId;
+        console.log("the row value is >>>" + rowData.entity.batchId);
+        var urldata = "/downloadCertificate/" + fileName;
+
+        window.open(urldata);
+    };
+});
