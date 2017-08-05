17124d914a2220d67111329fd811620959ff1d90
Alkesh <alkeshsrivastava11@gmail.com>
2017-08-05 16:39:02 +0530

    Author         : Alkesh Srivastava Files Added    : None Files Modified : accordion1.html, profileCreationTp.js Description    : Added functionality for profile creation for training partner(Front end)

--- skillIndia/server/src/main/resources/static/js/profileCreationTp.js
+++ skillIndia/server/src/main/resources/static/js/profileCreationTp.js
@@ -12,6 +12,17 @@ profileCreationTp.controller('profileCreationTp' , function($scope, $http){
     $scope.organizationName = "JSS";
     $scope.sPOCName = "Rakshit";
 
+    $scope.address = "Sacramento";
+    $scope.city = "Lucknow";
+    $scope.pincode = 5555555;
+    $scope.mobile = 1112223334;
+    $scope.mobileAlt = 7778889996;
+    $scope.landline = 8888888888;
+    $scope.landlineAlt = 0522888999;
+    $scope.fax = 4654681351;
+    $scope.website = "random@random.org";
+
+
     $scope.submit = function(){
         alert("Application filled successfully!")
         //POST function to post the data into backend
