170642d545a2007fe971ac78e51ecd1204b4c7b2
Alkesh <alkeshsrivastava11@gmail.com>
2017-08-06 20:08:30 +0530

    Author         : Alkesh Srivastava Files Added    : accordion3.html, accordion4.html Files Modified : accordion1.html, profileCreationTp.js, profileCreationTp.html, accordion2.html Description    : Added object bind method for data retrieval

--- skillIndia/server/src/main/resources/static/js/profileCreationTp.js
+++ skillIndia/server/src/main/resources/static/js/profileCreationTp.js
@@ -12,21 +12,61 @@ profileCreationTp.controller('profileCreationTp' , function($scope, $http){
     // Store response.data into trainingPartner
 
     $scope.states = ["UP", "Uttarakhand", "MP", "Bihar"];
-//    $scope.organizationName = "JSS";
-//    $scope.sPOCName = "Rakshit";
-//
-//
-//    $scope.address = "Sacramento";
-//    $scope.city = "Lucknow";
-//    $scope.pincode = 5555555;
-//    $scope.mobile = 1112223334;
-//    $scope.mobileAlt = 7778889996;
-//    $scope.landline = 8888888888;
-//    $scope.landlineAlt = 0522888999;
-//    $scope.fax = 4654681351;
-//    $scope.website = "random@random.org";
 
+    $scope.grantTable = {
+        enableGridMenus : false,
+        enableSorting: false,
+        enableFiltering: false,
+        enableCellEdit : true,
+        enableColumnMenus : false,
+        enableHorizontalScrollbar:0,
 
+        columnDefs:[
+          { name: 'SNo',           displayName: 'SNo.',              cellClass:'sno',  headerCellClass:'layer'},
+          { name: 'ministry',            displayName: 'Ministry from which grant was received ',          cellClass:'fname',headerCellClass:'File-Name'},
+          { name: 'natureOfWork',            displayName: 'Nature of Work',               cellClass:'type', headerCellClass:'Type'},
+          { name: 'remark',    displayName: 'Remarks',               cellClass:'date', headerCellClass:'Date'},
+       ]
+      };
+
+    $scope.directorTable = {
+          enableGridMenus : false,
+          enableSorting: false,
+          enableFiltering: false,
+          enableCellEdit : true,
+          enableColumnMenus : false,
+          enableHorizontalScrollbar:0,
+
+          columnDefs:[
+            { name: 'Name',           displayName: 'Name',              cellClass:'sno',  headerCellClass:'layer'},
+            { name: 'Designation',            displayName: 'Designation',          cellClass:'fname',headerCellClass:'File-Name'},
+            { name: 'EmailId',            displayName: 'Email-id',               cellClass:'type', headerCellClass:'Type'},
+            { name: 'ContactNumber',    displayName: 'Contact Number',               cellClass:'date', headerCellClass:'Date'},
+            { name: 'Educational Qualification',    displayName: 'Educational Qualification',               cellClass:'date', headerCellClass:'Date'},
+            { name: 'Experience',    displayName: 'Experience',               cellClass:'date', headerCellClass:'Date'},
+            { name: 'UploadCV',    displayName: 'Upload CV',               cellClass:'date', headerCellClass:'Date'},
+         ]
+        };
+
+    $scope.trainingStaffTable = {
+          enableGridMenus : false,
+          enableSorting: false,
+          enableFiltering: false,
+          enableCellEdit : true,
+          enableColumnMenus : false,
+          enableHorizontalScrollbar:0,
+
+          columnDefs:[
+            { name: 'Name',           displayName: 'Name',              cellClass:'sno',  headerCellClass:'layer'},
+            { name: 'Designation',            displayName: 'Designation',          cellClass:'fname',headerCellClass:'File-Name'},
+            { name: 'EmailId',            displayName: 'Email-id',               cellClass:'type', headerCellClass:'Type'},
+            { name: 'EducationalQualification',    displayName: 'Educational Qualification',               cellClass:'date', headerCellClass:'Date'},
+            { name: 'IndustrialExperience',    displayName: 'Industrial Experience',               cellClass:'date', headerCellClass:'Date'},
+            { name: 'Regular/Visiting',    displayName: 'Regular/Visiting',               cellClass:'date', headerCellClass:'Date'},
+            { name: 'UploadCV',    displayName: 'Upload CV',               cellClass:'date', headerCellClass:'Date'},
+            { name: 'UploadCertificate',    displayName: 'Upload Certificate',               cellClass:'date', headerCellClass:'Date'},
+         ]
+        };
     //response is the string that contains the nature of the button
     $scope.submit = function(response){
         alert("Application filled successfully! " + response);
