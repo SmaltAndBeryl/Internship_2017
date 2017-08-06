afc9b46677634d9fd04a7996b106c1b4e210f1a1
Alkesh <alkeshsrivastava11@gmail.com>
2017-08-06 11:48:35 +0530

    Author         : Alkesh Srivastava Files Added    : None Files Modified : accordion1.html, profileCreationTp.js, profileCreationTp.html, accordion2.html Description    : Added object bind method for data retrieval

--- skillIndia/server/src/main/resources/static/js/profileCreationTp.js
+++ skillIndia/server/src/main/resources/static/js/profileCreationTp.js
@@ -2,34 +2,40 @@ var profileCreationTp = angular.module('hello');
 
 profileCreationTp.controller('profileCreationTp' , function($scope, $http){
 
-
 //    $http.get('/url')
 //        .then(function(response){
 //
 //        });
 
+    var trainingPartner = {};
+
+    // Store response.data into trainingPartner
+
     $scope.states = ["UP", "Uttarakhand", "MP", "Bihar"];
-    $scope.organizationName = "JSS";
-    $scope.sPOCName = "Rakshit";
+//    $scope.organizationName = "JSS";
+//    $scope.sPOCName = "Rakshit";
+//
+//
+//    $scope.address = "Sacramento";
+//    $scope.city = "Lucknow";
+//    $scope.pincode = 5555555;
+//    $scope.mobile = 1112223334;
+//    $scope.mobileAlt = 7778889996;
+//    $scope.landline = 8888888888;
+//    $scope.landlineAlt = 0522888999;
+//    $scope.fax = 4654681351;
+//    $scope.website = "random@random.org";
 
-    $scope.address = "Sacramento";
-    $scope.city = "Lucknow";
-    $scope.pincode = 5555555;
-    $scope.mobile = 1112223334;
-    $scope.mobileAlt = 7778889996;
-    $scope.landline = 8888888888;
-    $scope.landlineAlt = 0522888999;
-    $scope.fax = 4654681351;
-    $scope.website = "random@random.org";
 
-
-    $scope.submit = function(){
-        alert("Application filled successfully!")
+    //response is the string that contains the nature of the button
+    $scope.submit = function(response){
+        alert("Application filled successfully! " + response);
         //POST function to post the data into backend
+
     };
 
-    $scope.save = function(){
-        alert("Data saved successfully..");
+    $scope.save = function(response){
+        alert("Data saved successfully.. " + response);
         //POST function to post into backend
     }
 });
