675e61579db0a5b977be22b1f24894eda6a3a24f
ruchitjain <ruchit.jain15@gmail.com>
2017-08-08 00:18:43 +0530

    Author : Ruchit Description : Profile Creation TP updated files

--- skillIndia/server/src/main/resources/static/js/profileCreationTp.js
+++ skillIndia/server/src/main/resources/static/js/profileCreationTp.js
@@ -7,7 +7,7 @@ profileCreationTp.controller('profileCreationTp' , function($scope, $http){
 //
 //        });
 
-    var trainingPartner = {};
+     $scope.trainingPartner = {};
 
     // Store response.data into trainingPartner
 
@@ -68,14 +68,167 @@ profileCreationTp.controller('profileCreationTp' , function($scope, $http){
          ]
         };
     //response is the string that contains the nature of the button
-    $scope.submit = function(response){
+    /*$scope.submit = function(response){
         alert("Application filled successfully! " + response);
         //POST function to post the data into backend
 
-    };
+    };*/
 
     $scope.save = function(response){
         alert("Data saved successfully.. " + response);
         //POST function to post into backend
-    }
+		
+
+        console.log("here in save as draft");
+
+
+        var manageControllerURI = "/saveAsDraftAndSubmit";
+        
+        console.log($scope.trainingPartner);
+        
+       /* $scope.trainingPartner = $scope.trainingPartner.concat([
+                                                  {type : "SaveAsDraft"}
+                                                ]);*/
+        
+        $scope.trainingPartner["type"] = "SaveAsDraft";
+        
+        console.log($scope.trainingPartner);
+
+        $http({
+        	url : manageControllerURI,
+        	method : "POST",
+        	data : $scope.trainingPartner
+        	/*data: angular.toJson(editdetailsOfApplication)*/
+        }).then(
+        		function(response)
+        		{
+        			console.log("SUCCESS 123");
+        			console.log("RESPONSE COMING FROM SERVER IS  : "+ response);
+        			
+        			console.log("RESPONSE COMING FROM SERVER IS  : "+ response.data);
+        		 if(response.data == "" )
+        			 {
+        			 console.log("NULL DATA I.E. ERROR");
+        			 }
+        		 else
+        			 console.log("ELSE");
+        			
+        /*			
+        			 $http.get('/approve')
+        	    	    .then(function(dataResponse){
+        	    	      $scope.gridOptions.data = dataResponse.data.submitted;
+        	    	      $scope.gridOptionsIncomplete.data = dataResponse.data.incomplete;
+        	    	      $scope.gridOptionsRejected.data = dataResponse.data.rejected;
+        	    	      $scope.gridOptionsApproved.data = dataResponse.data.approved;
+        	    	      
+        	    	      var message = response.data.successMessage;
+        					console.log(response.data);
+        					$scope.message = response.data.successMessage;
+        					$scope.messagealert= true;
+        					var success=$scope.message;
+        					console.log('THIS IS THE RESPONSE IN THE COMMENT:'+success);	
+        				    alert(success);
+        					  	    
+        	    	      
+        	    	  })*/
+        		},
+        		function(errorResponse, status)
+        		{
+        			
+        			/*$scope.message = response.data.errorMessage;
+        			$scope.messagealert= true;
+        			var failure=$scope.message;*/
+        			console.log('THIS IS THE RESPONSE IN THE COMMENT:'+failure);	
+        		  
+        			
+        		}
+        		)
+        };
+        
+        
+        $scope.submit = function(response){
+            alert("Data saved successfully.. " + response);
+            //POST function to post into backend
+    		
+
+            console.log("here in submit");
+
+
+            var manageControllerURI = "/saveAsDraftAndSubmit";
+            
+            console.log($scope.trainingPartner);
+            
+           /* $scope.trainingPartner = $scope.trainingPartner.concat([
+                                                      {type : "SaveAsDraft"}
+                                                    ]);*/
+            
+            $scope.trainingPartner["type"] = "Submit";
+            
+            console.log($scope.trainingPartner);
+
+            $http({
+            	url : manageControllerURI,
+            	method : "POST",
+            	data : $scope.trainingPartner
+            	/*data: angular.toJson(editdetailsOfApplication)*/
+            }).then(
+            		function(response)
+            		{
+            		 console.log("SUCCESS");
+            /*			
+            			 $http.get('/approve')
+            	    	    .then(function(dataResponse){
+            	    	      $scope.gridOptions.data = dataResponse.data.submitted;
+            	    	      $scope.gridOptionsIncomplete.data = dataResponse.data.incomplete;
+            	    	      $scope.gridOptionsRejected.data = dataResponse.data.rejected;
+            	    	      $scope.gridOptionsApproved.data = dataResponse.data.approved;
+            	    	      
+            	    	      var message = response.data.successMessage;
+            					console.log(response.data);
+            					$scope.message = response.data.successMessage;
+            					$scope.messagealert= true;
+            					var success=$scope.message;
+            					console.log('THIS IS THE RESPONSE IN THE COMMENT:'+success);	
+            				    alert(success);
+            					  	    
+            	    	      
+            	    	  })*/
+            		},
+            		function(errorResponse, status)
+            		{
+            			
+            			/*$scope.message = response.data.errorMessage;
+            			$scope.messagealert= true;
+            			var failure=$scope.message;*/
+            			console.log('THIS IS THE RESPONSE IN THE COMMENT:'+failure);	
+            		  
+            			
+            		}
+            		)
+            };
+        
+        
+        
+            $http.get('/getTrainingPartnerData')
+            .then(function(response){
+            	console.log("In Get  Function ");
+            	console.log(response.data);
+            	$scope.trainingPartner=response.data;
+              console.log($scope.trainingPartner);
+              console.log(response.data.pincode);
+              console.log($scope.trainingPartner.sPOCName);
+              console.log($scope.trainingPartner.pincode);
+              /*if($scope.profileTp.pincode==0)
+            	  $scope.profileTp.pincode=null;*/
+             
+          });
+        
+        
+        
+        
+        
+        
+        
+        
+        
 });
