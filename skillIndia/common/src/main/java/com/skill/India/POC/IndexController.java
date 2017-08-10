847c016ec7e4aaf955ee238132fd164d6b35873f
ruchipareek5 <ruchipareek5@gmail.com>
2017-08-10 15:10:31 +0530

    Author           :Ruchi Pareek Description      :UserIdCheckInTrainingPartnerRegistration uncomment the code. Files modified   :UserIdCheckInTrainingPartnerRegistration, IndexController

--- skillIndia/common/src/main/java/com/skill/India/POC/IndexController.java
+++ skillIndia/common/src/main/java/com/skill/India/POC/IndexController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class IndexController {
-    @RequestMapping("/index")
-    public String index(){
-        return "index2";
-    }
+//    @RequestMapping("/index")
+//    public String index(){
+//        return "index2";
+//    }
 }
