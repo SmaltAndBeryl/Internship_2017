f571cd6171490b6c6cad81478e806c14dab4177e
Alkesh <alkeshsrivastava11@gmail.com>
2017-08-19 16:04:02 +0530

    Author         : Alkesh Srivastava Files Added    : None Files Modified : ManageRegistrationsController.java Description    : Modified the controller/ManageRegistrationsController.java file for PDF generation

--- skillIndia/common/src/main/java/com/skill/India/controller/ManageRegistrationsController.java
+++ skillIndia/common/src/main/java/com/skill/India/controller/ManageRegistrationsController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 
+import net.sf.jasperreports.engine.JRException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -122,16 +123,12 @@ public class ManageRegistrationsController {
 	
     @Privilege(value={"SCGJ"})
     @RequestMapping("/cityData")
-    public Collection<DataBeanDto> dataBeanDtoCollection(){
+    public Collection<DataBeanDto> dataBeanDtoCollection()throws JRException{
     	LOGGER.info("In ManageRegistrationsController - dataBeanDtoCollection");
     	LOGGER.info("Request Received from front end to generate Collection from beans and get the required data for PDF creation");
     	LOGGER.info("Creating Collection to collect data for PDF creation");
     	LOGGER.info("Sending Request to Service");
-    	Collection<DataBeanDto> dataBeanDtos = dataBeanService.dataBeanDtoCollection();
-        dataBeanService.getDataBeanList();
-        LOGGER.info("Response received from Service");
-        LOGGER.info("Data received in Collection");
-//        dataBeanService.generatePdf();
+    	
         return dataBeanService.dataBeanDtoCollection();
     }
 
