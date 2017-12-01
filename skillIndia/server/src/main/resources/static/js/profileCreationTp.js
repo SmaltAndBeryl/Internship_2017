576f117a09ed6496977541056b2cd5d9f8a71b02
ruchipareek5 <ruchipareek5@gmail.com>
2017-12-01 18:21:30 +0530

    Author                 :Ruchi Pareek Description            :Changes on the front end to add center level details

--- skillIndia/server/src/main/resources/static/js/profileCreationTp.js
+++ skillIndia/server/src/main/resources/static/js/profileCreationTp.js
@@ -2,16 +2,6 @@ var profileCreationTp = angular.module('hello');
 
 profileCreationTp.controller('profileCreationTp', function($scope, $http, fileUpload) {
 
-    //    $http.get('/url')
-    //        .then(function(response){
-    //
-    //        });
-
-    //     $scope.trainingPartner;
-
-    // Store response.data into trainingPartner
-
-    //    $scope.TrainingPartnerOrganizationDetails;
 
     $http.get('/getDataNewUserProfileCreation')
         .then(function(response) {
@@ -22,16 +12,17 @@ profileCreationTp.controller('profileCreationTp', function($scope, $http, fileUp
             console.log("this is $scope.trainingPartner  " +  JSON.stringify($scope.trainingPartner));
             $scope.TrainingPartnerOrganizationDetails = $scope.trainingPartner.TrainingPartnerOrganizationDetails;
 
-//            $scope.spocName = $scope.TrainingPartnerOrganizationDetails.sPOCName;
-//            console.log("THE SPOC name is " + JSON.stringify($scope.spocName));
-//            console.log("The training partner is to be filled " + JSON.stringify($scope.trainingPartner));
-//            console.log("The training partner organization details are " + JSON.stringify($scope.trainingPartner.TrainingPartnerOrganizationDetails));
-//            console.log("The training partner organization details are " + JSON.stringify($scope.TrainingPartnerOrganizationDetails));
 
         });
     $scope.states = ["UP", "Uttarakhand", "MP", "Bihar"];
     $scope.type = ["regular", "visiting"];
-
+    $scope.pctpCenterDetails = [];
+    $scope.pctpOrgDetails = [];
+    $scope.pctpInstituteGrantDetail = [];
+    $scope.pctpRecognition = [];
+    $scope.pctpTrainingStaff = [];
+    $scope.pctpTrainingExperience = [];
+    
     $scope.addNew = function(experience) {
         console.log("data added successfully");
         $scope.trainingPartner.PriorExperienceDetails.push({
@@ -193,18 +184,18 @@ profileCreationTp.controller('profileCreationTp', function($scope, $http, fileUp
     }
     
     //Preview attachments
-    $scope.previewAttachments = function(event) {
-        var files = event.target.files;
-        $scope.attachments = [];
-        angular.forEach(files, function(file) {
-            $scope.attachments.push(file.name);
-            var reader = new FileReader();
-            reader.onload = function() {
-                $scope.$apply();
-            }
-            reader.readAsDataURL(file);
-        })
-    }
+//    $scope.previewAttachments = function(event) {
+//        var files = event.target.files;
+//        $scope.attachments = [];
+//        angular.forEach(files, function(file) {
+//            $scope.attachments.push(file.name);
+//            var reader = new FileReader();
+//            reader.onload = function() {
+//                $scope.$apply();
+//            }
+//            reader.readAsDataURL(file);
+//        })
+//    }
 
     $scope.grantTable = {
         enableGridMenus: false,
@@ -358,154 +349,34 @@ profileCreationTp.controller('profileCreationTp', function($scope, $http, fileUp
         //POST function to post the data into backend
 
     };*/
-
-    $scope.save = function(response) {
+    
+    
+    //Save Center details
+    $scope.saveCenter = function()
+    {
+    	console.log("Inside save center function");
     	
-    	$scope.dataPAN = new FormData();
-        var recordPan="PanNumber";
-        console.log("PanNumber :"+$scope.file);
+    	angular.foreach($scope.trainingPartner.TrainingPartnerCenterDetails, function(selected){
+    		selected.nameOfCenter
+    	})
+    	
+    	
+    	$scope.postValue = {
+                'type' : 'Draft',
+                'profileCreationTrainingPartnerOrganizationDetailsDto':  $scope.pctpOrgDetails,
+                'profileCreationTrainingPartnerCenterDetailsDto' :  $scope.pctpCenterDetails,
+                'profileCreationTrainingPartnerInstituteGrantDto' : $scope.pctpInstituteGrantDetail,
+                'profileCreationTrainingPartnerInstituteRecognitionDto' :$scope.pctpRecognition,
+                'profileCreationTrainingPartnerManagementAndStaffAndOfficialsDetailsDto': $scope.pctpTrainingStaff,
+                'profileCreationTrainingPartnerPriorExperienceInSkillTrainingDto' : $scope.pctpTrainingExperience
+            }
+    }
+    $scope.save = function(response) {
+    	var saveUrl = '/saveAsDraftAndSubmitTP';
+        var saveMethod = 'POST';
         
-        $scope.dataPAN.append("PanNumber",$scope.file);
-            console.log($scope.file);
-
-        $scope.pctpOrgDetails = {
-        		'trainingPartnerRegistrationId' :'1',
-    			'applicationId' : '2',
-    			'organizationName' : 'Smalt and Beryl',
-    			'sPOCName' : 'Ruchi Pareek',
-    			'address' : 'NH 24, near Vijay Nagar',
-    			'city':'Ghazibad',
-    			'state':'UP',
-    			'pincode':'201301',
-    			'mobileNumber':'87789979',
-    			'alternateMobileNumber':'98000909',
-    			'landlineNumber':'014123234',
-    			'alternateLandlineNumber':'014189890',
-    			'faxNumber':'014189890',
-    			'websites':'www.smaltandberyl.com',
-    			'yearOfEstablishment':'2012',
-    			'qualificationPacks':'abc',
-    			'qualificationPacksAnnexurePath':null,
-    			'nSDCFunded':'no',
-    			'nSDCFundedCertificatePath':null,
-    			'mediumOfInstructions':null,
-    			'selfOwnedInstitution':null,
-    			'selfOwnedInstitutionAnnexurePath':null,
-    			'franchiseOwnedInstitution':null,
-    			'franchiseOwnedInstitutionAnnexurePath':null,
-    			'mobileTrainingInstitution':'3',
-    			'mobileTrainingInstitutionAnnexurePath':null,
-    			'panNumber':'CIXPP90909',
-    			'panNumberPath':null,
-    			'tanNumber':'NI898989',
-    			'tanNumberPath':null,
-    			'turnOverOfInstitution':'56',
-    			'turnOverOfInstitutionBalanceSheetPath':null,
-    			'instituteReceivedAnyGrant':'no',
-    			'instituteReceivedAnyRecognition':'no',
-    			'priorExperienceOfInstitutionInSkillDevelopment':'12',
-    			'trainingStaffDetailsAnnexurePath':null
-        }
-        $scope.pctpCenterDetails =[
-                                   {
-                                	   'trainingPartnerCenterId' :1,
-                                	   'trainingPartnerRegistrationId' : 1,
-                                	   'nameOfCenter' : 'Smalt and beryl Business Solution center',
-                                	   'numberOfPermanentOfficeManager' : 1,
-                                	   'numberOftemporaryOfficeManager': null,
-                                	   'numberOfPermanentOfficeStaff' : null,
-                                	   'numberOfTemporaryOfficeStaff': 3,
-                                	   'numberOfPermanentLabAssistants' : 4,
-                                	   'numberOfTemporaryLabAssistants' : 56,
-                                	   'numberOfPermanentAccountants' :4,
-                                	   'numberOfTemporaryAccountants' :5,
-                                	   'numberOfPermanentSupportStaff' : 6,
-                                	   'numberOfTemporarySupportStaff' : 0,
-                                	   'numberOfPermanentOtherEmployees' : 8,
-                                	   'numberOfTemporaryOtherEmployees' :0,
-                                	   'areaOfInstitute':345,
-                                	   'buildingType' :self,
-                                	   'sizeOfClassrooms':45,
-                                	   'classroomPicsPath':null,
-                                	   'numberOfClassrooms': null,
-                                	   'sizeOfLabs': null,
-                                	   'labPicsPath': null,
-                                	   'numberOfLabs': 12,
-                                	   'sizeOfWorkshops':null,
-                                	   'workshopPicsPath':null,
-                                	   'numberOfWorkshops' :null,
-                                	   'mandatoryToolKitpresent':null,
-                                	   'mandatoryToolKitAnnexurePath':null,
-                                	   'mandatoryToolKitPicsPath':null,
-                                	   'safeDrinkingWater':null,
-                                	   'powerBackup':1,
-                                	   'separateToilets': 1,
-                                	   'transportFacility':1,
-                                	   'presenceOfLibrary':1,
-                                	   'numberOfTechnicalBooks':120,
-                                	   'numberOfNonTechnicalBooks':5,
-                                	   'numberOfMagazines': 5,
-                                	   'numberOfDailies':3,
-                                	   'remarksOnInfrastructureDetails':6,
-                                	   'sufficientClassroomIlluminationLevel':1,
-                                	   'sufficientClassroomVentilationLevel': 1,
-                                	   'sufficientCenterCleanliness':1,
-                                	   'centerWeatherProtected':1,
-                                	   'remarksOnLearningEnviornment':1,
-                                	   'printedBrochureOrProspectus':1,
-                                	   'documentedPolicyAndProcedures':1,
-                                	   'concessionPolicy':1,
-                                	   'safeCustodyOfStudentDocuments':1,
-                                	   'studentAgreementWithInstitution':1,
-                                	   'remarksOnStudentAdmissionDetails':1,
-                                	   'isActive':1
-                                   }
-                                 ]
-        
-        $scope.pctpInstituteGrantDetail = [
-        {
-        	'instituteGrantId': '1',
-        	'trainingPartnerRegistrationId':'1',
-        	'nameOfMinistry' : 'M.H.Rr.D',
-        	'natureOfWork': 'Aenwaii 2',
-        	'remarks': 'no remakrs 2',
-        	'isActive': true
-        }]
-        $scope.pctpRecognition = [{
-        	'trainingPartnerRegistrationId':1,
-        	'instituteRecognitionId' :1,
-        	'nameOfRecognizingBody': 'SSCGJJ',
-        	'recognitionNumber': '1213212',
-        	'yearOfRecognition':'2323',
-        	'validityOfRecognition':'33',
-        	'isActive': false
-        
-        }]
-        $scope.pctpTrainingStaff = [{
-        	'managementAndStaffId' : '1',
-        	'trainingPartnerRegistrationId': 1,
-        	'trainingPartnerCenterId':1,
-        	'type':'no type',
-        	'name':'Ruchi Pareek',
-        	'designation':'CLO',
-        	'emailId':'ruchi@smaltandberyl.com',
-        	'contactNumber':989898989,
-        	'educationalQualification': 'B Tech.',
-        	'regularOrVisiting': 'regular',
-        	'experience':'10',
-        	'isActive':1
-        	
-        }]
-        $scope.pctpTrainingExperience =[{
-        	'priorExperienceInSkillTrainingId' : '1',
-        	'trainingPartnerRegistrationId' : '1',
-        	'courseName' : 'Course Name 1',
-        	'numberOfBatchesPerYear' : '123',
-        	'numberOfStudentsInEachBatch' : '43',
-        	'isActive' : 1
-        }]
-        
-        $scope.postValue = {
+    	$scope.postValue = {
+    			
             'type' : 'Draft',
             'profileCreationTrainingPartnerOrganizationDetailsDto':  $scope.pctpOrgDetails,
             'profileCreationTrainingPartnerCenterDetailsDto' :  $scope.pctpCenterDetails,
@@ -514,8 +385,6 @@ profileCreationTp.controller('profileCreationTp', function($scope, $http, fileUp
             'profileCreationTrainingPartnerManagementAndStaffAndOfficialsDetailsDto': $scope.pctpTrainingStaff,
             'profileCreationTrainingPartnerPriorExperienceInSkillTrainingDto' : $scope.pctpTrainingExperience
         }
-        var saveUrl = '/saveAsDraftAndSubmitTP';
-        var saveMethod = 'POST';
         
         console.log("Preparing to post the values.............");
         $http({
