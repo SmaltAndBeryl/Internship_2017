5f8d0e0a523dbef272f1e99aab4a1c19c51e7e56
Pranjal Kumar Singh <pranjal@smaltandberyl.com>
2018-01-08 13:18:24 +0530

    Author              : Pranjal Kumar Singh Description         : Added sql file for creating SCGJ user.

---
+++ DB/SCGJ_USER.sql
@@ -0,0 +1,1 @@
+INSERT INTO `scgj`.`user` (`organizationName`, `sPOCName`, `userId`, `password`, `userRole`, `userStatus`) VALUES ('Skill Council of Green Jobs', 'Praveen Saxena', 'ceo@sscgj.in', 'ceo@123', 'SCGJ', 'registered');
